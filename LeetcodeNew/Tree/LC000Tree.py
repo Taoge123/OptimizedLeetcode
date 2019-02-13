cc4802f06947d38403e637ddafee10f73afb6c2d
taocheng <taocheng984@gmail.com>
2019-02-13 07:01:05 -0500

    basic traversal

--- LeetcodeNew/Tree/LC000Tree.py
+++ LeetcodeNew/Tree/LC000Tree.py
@@ -3,6 +3,25 @@ class TreeNode:
         self.val = x
         self.left = None
         self.right = None
+class SolutionInorder:
+    # @param {TreeNode} root
+    # @return {integer[]}
+    def inorderTraversal(self, root):
+        result, stack = [], [(root, False)]
+
+        while stack:
+            cur, visited = stack.pop()
+            if cur:
+                if visited:
+                    result.append(cur.val)
+                else:
+                    stack.append((cur.right, False))
+                    stack.append((cur, True))
+                    stack.append((cur.left, False))
+
+        return result
+
+
 
 class Solution:
     def inorderTraversal(self, root):
