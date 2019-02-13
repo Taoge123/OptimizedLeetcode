7dbda4f1f44449efe95a241b74e1f8281530ea8b
taocheng <taocheng984@gmail.com>
2019-02-12 20:34:16 -0500

    number of islands

---
+++ LeetcodeNew/DFS/LC_200_Number_of_Islands.py
@@ -0,0 +1,104 @@
+import collections
+
+class Solution:
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    #Replace all the 1s into #
+    def dfs(self, grid, i, j):
+        if i < 0 or j < 0 or i >= len(grid) or j >= len(grid[0]) or grid[i][j] != '1':
+            return
+        grid[i][j] = '#'
+        self.dfs(grid, i + 1, j)
+        self.dfs(grid, i - 1, j)
+        self.dfs(grid, i, j + 1)
+        self.dfs(grid, i, j - 1)
+
+
+
+class Solution2:
+    def is_valid(self, grid, r, c):
+        m, n = len(grid), len(grid[0])
+        if r < 0 or c < 0 or r >= m or c >= n:
+            return False
+        return True
+
+    def numIslandsDFS(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid or not grid[0]:
+            return 0
+
+        m, n = len(grid), len(grid[0])
+        count = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    self.dfs(grid, i, j)
+                    count += 1
+        return count
+
+    def dfs(self, grid, r, c):
+        grid[r][c] = '0'
+        directions = [(0,1), (0,-1), (-1,0), (1,0)]
+        for d in directions:
+            nr, nc = r + d[0], c + d[1]
+            if self.is_valid(grid, nr, nc) and grid[nr][nc] == '1':
+                self.dfs(grid, nr, nc)
+
+    def numIslandsBFS(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid or not grid[0]:
+            return 0
+
+        m, n = len(grid), len(grid[0])
+        count = 0
+        for i in xrange(m):
+            for j in xrange(n):
+                if grid[i][j] == '1':
+                    self.bfs(grid, i, j)
+                    count += 1
+        return count
+
+    def bfs(self, grid, r, c):
+        queue = collections.deque()
+        queue.append((r, c))
+        grid[r][c] = '0'
+        while queue:
+            directions = [(0,1), (0,-1), (-1,0), (1,0)]
+            r, c = queue.popleft()
+            for d in directions:
+                nr, nc = r + d[0], c + d[1]
+                if self.is_valid(grid, nr, nc) and grid[nr][nc] == '1':
+                    queue.append((nr, nc))
+                    grid[nr][nc] = '0'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
