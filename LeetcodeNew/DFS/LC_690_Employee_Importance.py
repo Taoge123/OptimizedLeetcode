b8fd1486d3b59e3e48c2715a956817f7d5cca42e
taocheng <taocheng984@gmail.com>
2019-02-18 18:23:48 -0500

    LC_690_Employee_Importance.py

---
+++ LeetcodeNew/DFS/LC_690_Employee_Importance.py
@@ -0,0 +1,63 @@
+"""
+题目意思就是题目自定了一个数据结构，第一个值为id，第二个值为权重，第三个值为下属
+一个公司每个人对应一个数据结构，给你一个人的id，让你返回他以及他的所有下属的权重和。
+
+"""
+
+
+# Employee info
+class Employee(object):
+    def __init__(self, id, importance, subordinates):
+        # It's the unique id of each node.
+        # unique id of this employee
+        self.id = id
+        # the importance value of this employee
+        self.importance = importance
+        # the id of direct subordinates
+        self.subordinates = subordinates
+
+
+class Solution(object):
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+        a = []
+        sum = 0
+        a.append(id)
+        while a:
+            id = a[0]
+
+            sum = employees[id - 1][1] + sum
+            if employees[id - 1][2]:
+                for i in range(len(employees[id - 1][2])):
+                    a.append(employees[id - 1][2][i])
+            a.remove(id)
+
+
+class Solution2(object):
+    def getImportance(self, employees, id):
+        """
+        :type employees: Employee
+        :type id: int
+        :rtype: int
+        """
+        # Time: O(n)
+        # Space: O(n)
+        emps = {employee.id: employee for employee in employees}
+        def dfs(id):
+            subordinates_importance = sum([dfs(sub_id) for sub_id in emps[id].subordinates])
+            return subordinates_importance + emps[id].importance
+        return dfs(id)
+
+
+
+
+
+
+
+
+
+
