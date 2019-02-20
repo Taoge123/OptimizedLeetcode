0343a4de966c260d5ffc285b98e54acadde295bb
taocheng <taocheng984@gmail.com>
2019-02-19 20:00:51 -0500

    LC_737_Sentence_Similarity_II.py

--- LeetcodeNew/DFS/LC_737_Sentence_Similarity_II.py
+++ LeetcodeNew/DFS/LC_737_Sentence_Similarity_II.py
@@ -6,6 +6,114 @@ stack will contain all the nodes that are queued up for processing,
 while seen will be all the nodes that have been queued for processing
 (whether they have been processed or not).
 """
+
+
+# union find set
+class SolutionUF:
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :type pairs: List[List[str]]
+        :rtype: bool
+        """
+        if len(words1) != len(words2):
+            return False
+        # construct union find set
+        self.parents = {}
+        for pair in pairs:
+            if pair[0] not in self.parents:
+                self.parents[pair[0]] = pair[0]
+            if pair[1] not in self.parents:
+                self.parents[pair[1]] = pair[1]
+        for pair in pairs:
+            self.union(pair[0], pair[1])
+
+        # perform union find
+        for i in range(len(words1)):
+            if words1[i] == words2[i]:
+                continue
+            elif words1[i] not in self.parents or words2[i] not in self.parents:
+                return False
+            elif self.find(words1[i]) != self.find(words2[i]):
+                return False
+        return True
+
+    def find(self, word):
+        if self.parents[word] != word:
+            p = self.find(self.parents[self.parents[word]])
+            self.parents[word] = p
+        return self.parents[word]
+
+    def union(self, w1, w2):
+        p1 = self.find(w1)
+        p2 = self.find(w2)
+        if p1 != p2:
+            self.parents[p2] = p1
+
+
+# based on the above solution, add union by rank
+class SolutionUFRank:
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :type pairs: List[List[str]]
+        :rtype: bool
+        """
+        if len(words1) != len(words2):
+            return False
+        # construct union find set
+        self.parents = {}
+        self.rank = {}
+        for pair in pairs:
+            if pair[0] not in self.parents:
+                self.parents[pair[0]] = pair[0]
+                self.rank[pair[0]] = 0
+            if pair[1] not in self.parents:
+                self.parents[pair[1]] = pair[1]
+                self.rank[pair[1]] = 0
+        for pair in pairs:
+            self.union(pair[0], pair[1])
+
+        # perform union find
+        for i in xrange(len(words1)):
+            if words1[i] == words2[i]:
+                continue
+            elif words1[i] not in self.parents or words2[i] not in self.parents:
+                return False
+            elif self.find(words1[i]) != self.find(words2[i]):
+                return False
+        return True
+
+    def find(self, word):
+        if self.parents[word] != word:
+            p = self.find(self.parents[self.parents[word]])
+            self.parents[word] = p
+        return self.parents[word]
+
+    def union(self, w1, w2):
+        p1 = self.find(w1)
+        p2 = self.find(w2)
+        if p1 != p2:
+            if self.rank[p1] > self.rank[p2]:
+                self.parents[p2] = p1
+            elif self.rank[p1] < self.rank[p2]:
+                self.parents[p1] = p2
+            else:
+                self.parents[p2] = p1
+                self.rank[p1] += 1
+
+
+
+
+
+
+
+
+
+
+
 import collections
 import itertools
 
@@ -152,4 +260,68 @@ class Solution4:
             if dsu.find(word2id[w1]) != dsu.find(word2id[w2]):  # 只要判断两个单词的id 是否指向共同的parent
                 return False
         return True
+
+
+
+class Solution5:
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
+        """
+        :type words1: List[str]
+        :type words2: List[str]
+        :type pairs: List[List[str]]
+        :rtype: bool
+        """
+        if len(words1) != len(words2): return False
+        similars = collections.defaultdict(set)
+        for w1, w2 in pairs:
+            similars[w1].add(w2)
+            similars[w2].add(w1)
+
+        def dfs(words1, words2, visits):
+            for similar in similars[words2]:
+                if words1 == similar:
+                    return True
+                elif similar not in visits:
+                    visits.add(similar)
+                    if dfs(words1, similar, visits):
+                        return True
+            return False
+
+        for w1, w2 in zip(words1, words2):
+            if w1 != w2 and not dfs(w1, w2, set([w2])):
+                return False
+        return True
+
+"""
+http://www.cnblogs.com/grandyang/p/8053934.html
+
+这道题是之前那道Sentence Similarity的拓展，那道题说单词之间不可传递，
+于是乎这道题就变成可以传递了，那么难度就增加了。不过没有关系，还是用我们的经典老三样来解，
+BFS，DFS，和Union Find。我们先来看BFS的解法，其实这道题的本质是无向连通图的问题，
+那么首先要做的就是建立这个连通图的数据结构，对于每个结点来说，我们要记录所有和其相连的结点，
+所以我们建立每个结点和其所有相连结点集合之间的映射，
+比如对于这三个相似对(a, b), (b, c)，和(c, d)，我们有如下的映射关系：
+
+a -> {b}
+
+b -> {a, c}
+
+c -> {b, d}
+
+d -> {c}
+
+那么如果我们要验证a和d是否相似，就需要用到传递关系，a只能找到b，b可以找到a，c，为了不陷入死循环，
+我们将访问过的结点加入一个集合visited，那么此时b只能去，c只能去d，那么说明a和d是相似的了。
+那么我们用for循环来比较对应位置上的两个单词，如果二者相同，那么直接跳过去比较接下来的。
+否则就建一个访问即可visited，建一个队列queue，然后把words1中的单词放入queue，
+建一个布尔型变量succ，标记是否找到，然后就是传统的BFS遍历的写法了，从队列中取元素，
+如果和其相连的结点中有words2中的对应单词，标记succ为true，并break掉。
+否则就将取出的结点加入队列queue，并且遍历其所有相连结点，
+将其中未访问过的结点加入队列queue继续循环
+"""
+
+
+
+
+
 
