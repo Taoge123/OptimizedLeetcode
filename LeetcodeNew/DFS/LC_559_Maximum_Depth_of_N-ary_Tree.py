4b027553032b09bcb3c18a317ab17df0a1392b2b
taocheng <taocheng984@gmail.com>
2019-02-20 23:02:17 -0500

    LC_559_Maximum_Depth_of_N-ary_Tree.py

---
+++ LeetcodeNew/DFS/LC_559_Maximum_Depth_of_N-ary_Tree.py
@@ -0,0 +1,39 @@
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        elif root.children == []:
+            return 1
+        else:
+            height = [self.maxDepth(c) for c in root.children]
+            return max(height) + 1
+
+
+class Solution2:
+    def maxDepth(self, root):
+        """
+        :type root: Node
+        :rtype: int
+        """
+        stack = []
+        if root is not None:
+            stack.append((1, root))
+
+        depth = 0
+        while stack != []:
+            current_depth, root = stack.pop()
+            if root is not None:
+                depth = max(depth, current_depth)
+                for c in root.children:
+                    stack.append((current_depth + 1, c))
+
+        return depth
+
+
+
+
