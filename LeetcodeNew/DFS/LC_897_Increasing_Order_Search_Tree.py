351f35eb0f07ecc681c3bf690f522a66d30a465f
taocheng <taocheng984@gmail.com>
2019-03-03 10:37:28 -0500

    LC_897_Increasing_Order_Search_Tree.py

--- LeetcodeNew/DFS/LC_897_Increasing_Order_Search_Tree.py
+++ LeetcodeNew/DFS/LC_897_Increasing_Order_Search_Tree.py
@@ -16,6 +16,36 @@ Otherwise it will be TLE for Leetcode to traverse your tree.
 Should arrange the old tree, not create a new tree.
 The judgement won't take it as wrong answer, but it is.
 
+Example 1:
+Input: [5,3,6,2,4,null,8,1,null,null,null,7,9]
+
+       5
+      / \
+    3    6
+   / \    \
+  2   4    8
+ /        / \
+1        7   9
+
+Output: [1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+
+ 1
+  \
+   2
+    \
+     3
+      \
+       4
+        \
+         5
+          \
+           6
+            \
+             7
+              \
+               8
+                \
+                 9  
 """
 
 
