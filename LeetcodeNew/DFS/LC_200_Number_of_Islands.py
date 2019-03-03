c0035d315331491c516d6b8a3c3edf93ce6452f0
taocheng <taocheng984@gmail.com>
2019-03-03 10:11:54 -0500

    LC_200_Number_of_Islands.py

--- LeetcodeNew/DFS/LC_200_Number_of_Islands.py
+++ LeetcodeNew/DFS/LC_200_Number_of_Islands.py
@@ -1,3 +1,7 @@
+#https://leetcode.com/problems/number-of-islands/discuss/121174/Python-Union-Find-solution-with-both-path-compression-and-union-by-rank
+#https://leetcode.com/problems/number-of-islands/discuss/56519/Union-Find-in-Python
+
+
 import collections
 
 class Solution:
@@ -91,7 +95,63 @@ class Solution2:
 
 
 
+class UnionFind(object):
+    def __init__(self, grid):
+        m, n = len(grid), len(grid[0])
+        self.count = 0
+        self.parent = [-1] * (m*n)
+        self.rank = [0] * (m*n)
+        for i in xrange(m):
+            for j in xrange(n):
+                if grid[i][j] == '1':
+                    self.parent[i*n + j] = i*n + j
+                    self.count += 1
 
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, x, y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+        if rootx != rooty:
+            if self.rank[rootx] > self.rank[rooty]:
+                self.parent[rooty] = rootx
+            elif self.rank[rootx] < self.rank[rooty]:
+                self.parent[rootx] = rooty
+            else:
+                self.parent[rooty] = rootx
+                self.rank[rootx] += 1
+            self.count -= 1
+
+class SolutionUF:
+    def is_valid(self, grid, r, c):
+        m, n = len(grid), len(grid[0])
+        if r < 0 or c < 0 or r >= m or c >= n:
+            return False
+        return True
+
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        if not grid or not grid[0]:
+            return 0
+
+        uf = UnionFind(grid)
+
+        directions = [(0,1), (0,-1), (-1,0), (1,0)]
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    for d in directions:
+                        nr, nc = i + d[0], j + d[1]
+                        if self.is_valid(grid, nr, nc) and grid[nr][nc] == '1':
+                            uf.union(i*n+j, nr*n+nc)
+        return uf.count
 
 
 
