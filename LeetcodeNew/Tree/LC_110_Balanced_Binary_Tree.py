20a768e2afa13822f7042a4fcb3b8dc29ccff301
taocheng <taocheng984@gmail.com>
2019-03-03 20:37:30 -0500

    LC_110_Balanced_Binary_Tree.py

--- LeetcodeNew/Tree/LC_110_Balanced_Binary_Tree.py
+++ LeetcodeNew/Tree/LC_110_Balanced_Binary_Tree.py
@@ -15,6 +15,19 @@ class Solution:
         return max(left, right) + 1
 
 
+class SolutionOptimized:
+    def isBalanced(self, root):
+
+        def check(root):
+            if root is None:
+                return 0
+            left = check(root.left)
+            right = check(root.right)
+            if left == -1 or right == -1 or abs(left - right) > 1:
+                return -1
+            return 1 + max(left, right)
+
+        return check(root) != -1
 
 """
 110. Balanced Binary Tree
