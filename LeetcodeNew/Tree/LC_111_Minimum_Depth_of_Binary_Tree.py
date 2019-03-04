bc40e739fc827f9c4367ae8497e7a374d4936365
taocheng <taocheng984@gmail.com>
2019-03-03 22:11:01 -0500

    LC_111_Minimum_Depth_of_Binary_Tree.py

---
+++ LeetcodeNew/Tree/LC_111_Minimum_Depth_of_Binary_Tree.py
@@ -0,0 +1,101 @@
+
+"""
+https://leetcode.com/problems/minimum-depth-of-binary-tree/discuss/158303/Python-or-DFS-tm
+
+这里思路很简单，分制以后再返回的时候增加层级。
+唯一要注意的是一定要有个Edge情况，如果左右孩子一边为空，例如地下白板的例子，
+就不能返回min(left, right)，因为空的一边为0，所以会返回错误的数。
+这种edge我们则返回: left + right + 1
+
+"""
+
+class Solution:
+    def minDepth(self, root):
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+        if root.left and not root.right:
+            return 1 + self.minDepth(root.left)
+        if root.right and not root.left:
+            return 1 + self.minDepth(root.right)
+        return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
+
+
+class SolutionGood:
+    def minDepth(self, root):
+        if not root:
+            return 0
+        left = self.minDepth(root.left)
+        right = self.minDepth(root.right)
+        if left == 0 or right == 0:
+            return left + right + 1
+        return min(left, right) + 1
+
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+
+        children = [root.left, root.right]
+        # if we're at leaf node
+        if not any(children):
+            return 1
+
+        min_depth = float('inf')
+        for c in children:
+            if c:
+                min_depth = min(self.minDepth(c), min_depth)
+        return min_depth + 1
+
+
+class Solution2:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        else:
+            stack, min_depth = [(1, root), ], float('inf')
+
+        while stack:
+            depth, root = stack.pop()
+            children = [root.left, root.right]
+            if not any(children):
+                min_depth = min(depth, min_depth)
+            for c in children:
+                if c:
+                    stack.append((depth + 1, c))
+
+        return min_depth
+
+
+from collections import deque
+
+
+class Solution3:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        else:
+            node_deque = deque([(1, root), ])
+
+        while node_deque:
+            depth, root = node_deque.popleft()
+            children = [root.left, root.right]
+            if not any(children):
+                return depth
+            for c in children:
+                if c:
+                    node_deque.append((depth + 1, c))
