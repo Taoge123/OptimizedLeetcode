3e42a751ccb84a7dce6267d96031bb250812b666
taocheng <taocheng984@gmail.com>
2019-03-03 22:12:03 -0500

    LC_111_Minimum_Depth_of_Binary_Tree.py

--- LeetcodeNew/Tree/LC_111_Minimum_Depth_of_Binary_Tree.py
+++ LeetcodeNew/Tree/LC_111_Minimum_Depth_of_Binary_Tree.py
@@ -22,6 +22,22 @@ class Solution:
         return 1 + min(self.minDepth(root.left), self.minDepth(root.right))
 
 
+class Solution2:
+    # @param root, a tree node
+    # @return an integer
+    def minDepth(self, root):
+        if not root:
+            return 0
+        else:
+            # if one of the subtree is None, you should return the depth of another subtree.
+            # if all of the subtree is not None, you should return the minimum depth of the two subtrees
+            if root.left is None:
+                return self.minDepth(root.right) + 1
+            elif root.right is None:
+                return self.minDepth(root.left) + 1
+            else:
+                return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
+
 class SolutionGood:
     def minDepth(self, root):
         if not root:
