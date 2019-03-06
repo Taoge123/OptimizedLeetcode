6574db76976d0ffaa0b988facbed7e3e61a73b5e
taocheng <taocheng984@gmail.com>
2019-03-05 21:15:23 -0500

    LC_199_Binary_Tree_Right_Side_View.py

--- LeetcodeNew/Tree/LC_199_Binary_Tree_Right_Side_View.py
+++ LeetcodeNew/Tree/LC_199_Binary_Tree_Right_Side_View.py
@@ -51,8 +51,54 @@ class Solution:
         self.dfs(node.left, depth + 1)
 
 
-        
+
+
+class SolutionDFS:
+    def rightSideView(self, root):
+        rightmost_value_at_depth = dict() # depth -> node.val
+        max_depth = -1
+
+        stack = [(root, 0)]
+        while stack:
+            node, depth = stack.pop()
+
+            if node is not None:
+                # maintain knowledge of the number of levels in the tree.
+                max_depth = max(max_depth, depth)
+
+                # only insert into dict if depth is not already present.
+                rightmost_value_at_depth.setdefault(depth, node.val)
+
+                stack.append((node.left, depth+1))
+                stack.append((node.right, depth+1))
+
+        return [rightmost_value_at_depth[depth] for depth in range(max_depth+1)]
+
+
+from collections import deque
+
+class SolutionBFS:
+    def rightSideView(self, root):
+        rightmost_value_at_depth = dict() # depth -> node.val
+        max_depth = -1
+
+        queue = deque([(root, 0)])
+        while queue:
+            node, depth = queue.popleft()
+
+            if node is not None:
+                # maintain knowledge of the number of levels in the tree.
+                max_depth = max(max_depth, depth)
+
+                # overwrite rightmost value at current depth. the correct value
+                # will never be overwritten, as it is always visited last.
+                rightmost_value_at_depth[depth] = node.val
+
+                queue.append((node.left, depth+1))
+                queue.append((node.right, depth+1))
+
+        return [rightmost_value_at_depth[depth] for depth in range(max_depth+1)]
 
 
 
-
+    
