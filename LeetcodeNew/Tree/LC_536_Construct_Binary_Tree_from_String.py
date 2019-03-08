c46ef3defdedcfdfa99acfb8909d4eda9cf5e15e
taocheng <taocheng984@gmail.com>
2019-03-07 22:39:07 -0500

    LC_536_Construct_Binary_Tree_from_String.py

--- LeetcodeNew/Tree/LC_536_Construct_Binary_Tree_from_String.py
+++ LeetcodeNew/Tree/LC_536_Construct_Binary_Tree_from_String.py
@@ -1,6 +1,8 @@
 
-
 """
+https://www.geeksforgeeks.org/construct-binary-tree-string-bracket-representation/
+
+
 Example:
 Input: "4(2(3)(1))(6(5))"
 Output: return the tree root node representing the following tree:
@@ -102,5 +104,31 @@ class Solution2:
 
 
 
+
+class Solution3:
+    def str2tree(self, s):
+        """
+        :type s: str
+        :rtype: TreeNode
+        """
+        if not s: return None
+        n = ''
+        while s and s[0] not in ('(', ')'):
+            n += s[0]
+            s = s[1:]
+        node = TreeNode(int(n))
+        left, right = self.divide(s)
+        node.left = self.str2tree(left[1:-1])
+        node.right = self.str2tree(right[1:-1])
+        return node
+
+    def divide(self, s):
+        part, deg = '', 0
+        while s:
+            deg += {'(' : 1, ')' : -1}.get(s[0], 0)
+            part += s[0]
+            s = s[1:]
+            if deg == 0: break
+        return part, s
 
 
