ca481874fca5035efc499e81560bdd4a81f8a940
taocheng <taocheng984@gmail.com>
2019-03-08 19:01:44 -0500

    LC_572_Subtree_of_Another_Tree.py

--- LeetcodeNew/Tree/LC_572_Subtree_of_Another_Tree.py
+++ LeetcodeNew/Tree/LC_572_Subtree_of_Another_Tree.py
@@ -46,9 +46,7 @@ class Solution0:
     def isMatch(self, s, t):
         if not(s and t):
             return s is t
-        return (s.val == t.val and
-                self.isMatch(s.left, t.left) and
-                self.isMatch(s.right, t.right))
+        return (s.val == t.val and self.isMatch(s.left, t.left) and self.isMatch(s.right, t.right))
 
     def isSubtree(self, s, t):
         if self.isMatch(s, t): return True
