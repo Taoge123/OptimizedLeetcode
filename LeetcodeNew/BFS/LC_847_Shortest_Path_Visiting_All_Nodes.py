9bcc778e19dad7722e290339ee505d97acd9c414
taocheng <taocheng984@gmail.com>
2019-03-19 21:11:53 -0400

    LC_847_Shortest_Path_Visiting_All_Nodes.py

--- LeetcodeNew/BFS/LC_847_Shortest_Path_Visiting_All_Nodes.py
+++ LeetcodeNew/BFS/LC_847_Shortest_Path_Visiting_All_Nodes.py
@@ -116,3 +116,49 @@ class Solution4:
                     queue.appendleft((dist+1, next_node, state | 1 << next_node))
         return -1
 
+
+
+class Solution5:
+    def shortestPathLength(self, graph):
+        N = len(graph)
+        queue = collections.deque((1 << x, x) for x in range(N))
+        dist = collections.defaultdict(lambda: N*N)
+        for x in range(N): dist[1 << x, x] = 0
+
+        while queue:
+            cover, head = queue.popleft()
+            d = dist[cover, head]
+            if cover == 2**N - 1: return d
+            for child in graph[head]:
+                cover2 = cover | (1 << child)
+                if d + 1 < dist[cover2, child]:
+                    dist[cover2, child] = d + 1
+                    queue.append((cover2, child))
+
+
+
+class Solution6:
+    def shortestPathLength(self, graph):
+        N = len(graph)
+        dist = [[float('inf')] * N for i in range(1 << N)]
+        for x in range(N):
+            dist[1<<x][x] = 0
+
+        for cover in range(1 << N):
+            repeat = True
+            while repeat:
+                repeat = False
+                for head, d in enumerate(dist[cover]):
+                    for nei in graph[head]:
+                        cover2 = cover | (1 << nei)
+                        if d + 1 < dist[cover2][nei]:
+                            dist[cover2][nei] = d + 1
+                            if cover == cover2:
+                                repeat = True
+
+        return min(dist[2**N - 1])
+
+
+
+
+
