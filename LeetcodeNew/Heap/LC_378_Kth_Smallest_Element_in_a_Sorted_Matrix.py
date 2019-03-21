f11d6c8ed87d50ea57a382303ec712df4d4e35b8
taocheng <taocheng984@gmail.com>
2019-03-21 19:39:39 -0400

    LC_378_Kth_Smallest_Element_in_a_Sorted_Matrix.py

--- LeetcodeNew/Heap/LC_378_Kth_Smallest_Element_in_a_Sorted_Matrix.py
+++ LeetcodeNew/Heap/LC_378_Kth_Smallest_Element_in_a_Sorted_Matrix.py
@@ -132,6 +132,88 @@ class SolutionBinerySearch2:
         return cnt
 
 
+"""
+我觉得用二分做最好，这个方法只要求行有序，和列有木有序并没有关系。 
+（或者列有序，行有序无序都没关系）
+
+设L = min(matrix) R= max(matrix)  , mid =( L + R ) / 2 ，mid为我们猜测的答案。
+
+然后对于每一行，找它在该行中第几大（也是二分，找上界），累加和K比较。
+
+值得注意的是枚举 答案应该用下界， 因为猜想的解不一定在数组中，不断的收缩直到找到在数组中的元素为止。
+
+查找一行需要log(n) ，有n行所以nlog(n)，最坏下需要查找log(X)次
+（X= int最大值的时候logX仅仅才为32），X为最大最小数差值. 所以总复杂度为O(nlogn *  logX)
+
+PS：其实是我一开始看成就行有序→_→，然后就直接二分了
+"""
+
+class Solution11:
+    def kthSmallest(self, matrix, k):
+
+        n = len(matrix)
+        L, R = matrix[0][0], matrix[n - 1][n - 1]
+        while L < R:
+            mid = L + ((R - L) >> 1)
+            temp = sum([self.binary_search(row, mid, n) for row in matrix])
+            if temp < k:  L = mid + 1
+            else:  R = mid
+        return L
+
+    def binary_search(self, row, x, n):
+        L, R = 0, n
+        while L < R:
+            mid = (L + R) >> 1
+            if row[mid] <= x:
+                L = mid + 1
+            else:
+                R = mid
+        return L
+
+"""
+上述的解法并没有利用到列有序的性质。
+
+而下面的解法利用了列有序的性质，并将复杂度降到了O(nlogX) 其中X = max – min
+
+我们仍采用猜测法，设L = min(matrix) R= max(matrix) , mid =( L + R ) / 2 ，mid为我们猜测的答案。
+
+对于mid，我们不必再所有的行或列种执行二分查找，我们可以从左下角出发，若matrix[i][j] <= mid，
+则下一次查询在右边（j++），并且，该列的所有元素均比mid小，因此可以cnt += (i+1)
+
+对于matrix[i][j] > mid，则 i – – 。 过程类似于240. Search a 2D Matrix II  (题解在最下方)
+"""
+
+
+class Solution(object):
+    def kthSmallest(self, matrix, k):
+        """
+        :type matrix: List[List[int]]
+        :type k: int
+        :rtype: int
+        """
+        n = len(matrix)
+        L, R = matrix[0][0], matrix[n - 1][n - 1]
+        while L < R:
+            mid = L + ((R - L) >> 1)
+            temp = self.search_lower_than_mid(matrix, n, mid)
+            if temp < k:
+                L = mid + 1
+            else:
+                R = mid
+        return L
+
+    def search_lower_than_mid(self, matrix, n, x):
+        i, j = n - 1, 0
+        cnt = 0
+        while i >= 0 and j < n:
+            if matrix[i][j] <= x:
+                j += 1
+                cnt += i + 1
+            else:
+                i -= 1
+        return cnt
+
+
 
 matrix = [
    [ 1,  5,  9],
