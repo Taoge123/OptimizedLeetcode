9b9d5c0f21dffd655a2a6d618766299b130aad33
taocheng <taocheng984@gmail.com>
2019-03-21 23:09:59 -0400

    LC_253_Meeting_Rooms_II.py

---
+++ LeetcodeNew/Heap/LC_253_Meeting_Rooms_II.py
@@ -0,0 +1,178 @@
+
+"""
+https://leetcode.com/problems/meeting-rooms-ii/solution/
+
+Given an array of meeting time intervals consisting of start
+and end times [[s1,e1],[s2,e2],...] (si < ei),
+find the minimum number of conference rooms required.
+
+Example 1:
+
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
+Example 2:
+
+Input: [[7,10],[2,4]]
+Output: 1
+"""
+
+# Definition for an interval.
+import heapq
+
+class Interval:
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
+
+
+"""
+ # Very similar with what we do in real life. Whenever you want to start a meeting, 
+ # you go and check if any empty room available (available > 0) and
+ # if so take one of them ( available -=1 ). Otherwise,
+ # you need to find a new room someplace else ( numRooms += 1 ).  
+ # After you finish the meeting, the room becomes available again ( available += 1 ).
+ """
+
+class Solution11:
+    def minMeetingRooms(self, intervals):
+        starts = []
+        ends = []
+        for i in intervals:
+            starts.append(i.start)
+            ends.append(i.end)
+
+        starts.sort()
+        ends.sort()
+        s = e = 0
+        numRooms = available = 0
+        while s < len(starts):
+            if starts[s] < ends[e]:
+                if available == 0:
+                    numRooms += 1
+                else:
+                    available -= 1
+
+                s += 1
+            else:
+                available += 1
+                e += 1
+
+        return numRooms
+
+
+class Solution22:
+    def minMeetingRooms(self, intervals):
+        intervals.sort(lambda x, y: x.start - y.start)
+        ans, pq = 0, []
+        for interval in intervals:
+            while pq and pq[0] <= interval.start:
+                heapq.heappop(pq)
+            heapq.heappush(pq, interval.end)
+            ans = max(ans, len(pq))
+        return ans
+
+
+class SolutionCaikehe:
+    def minMeetingRooms(self, intervals):
+        intervals.sort(key=lambda x: x.start)
+        heap = []  # stores the end time of intervals
+        for i in intervals:
+            if heap and i.start >= heap[0]:
+                # means two intervals can use the same room
+                heapq.heapreplace(heap, i.end)
+            else:
+                # a new room is allocated
+                heapq.heappush(heap, i.end)
+        return len(heap)
+
+
+class Solution1:
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+
+        # If there is no meeting to schedule then no room needs to be allocated.
+        if not intervals:
+            return 0
+
+        # The heap initialization
+        free_rooms = []
+
+        # Sort the meetings in increasing order of their start time.
+        intervals.sort(key= lambda x: x.start)
+
+        # Add the first meeting. We have to give a new room to the first meeting.
+        heapq.heappush(free_rooms, intervals[0].end)
+
+        # For all the remaining meeting rooms
+        for i in intervals[1:]:
+
+            # If the room due to free up the earliest is free, assign that room to this meeting.
+            if free_rooms[0] <= i.start:
+                heapq.heappop(free_rooms)
+
+            # If a new room is to be assigned, then also we add to the heap,
+            # If an old room is allocated, then also we have to add to the heap with updated end time.
+            heapq.heappush(free_rooms, i.end)
+
+        # The size of the heap tells us the minimum rooms required for all the meetings.
+        return len(free_rooms)
+
+
+
+
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution2:
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+
+        # If there are no meetings, we don't need any rooms.
+        if not intervals:
+            return 0
+
+        used_rooms = 0
+
+        # Separate out the start and the end timings and sort them individually.
+        start_timings = sorted([i.start for i in intervals])
+        end_timings = sorted(i.end for i in intervals)
+        L = len(intervals)
+
+        # The two pointers in the algorithm: e_ptr and s_ptr.
+        end_pointer = 0
+        start_pointer = 0
+
+        # Until all the meetings have been processed
+        while start_pointer < L:
+            # If there is a meeting that has ended by the time the meeting at `start_pointer` starts
+            if start_timings[start_pointer] >= end_timings[end_pointer]:
+                # Free up a room and increment the end_pointer.
+                used_rooms -= 1
+                end_pointer += 1
+
+            # We do this irrespective of whether a room frees up or not.
+            # If a room got free, then this used_rooms += 1 wouldn't have any effect. used_rooms would
+            # remain the same in that case. If no room was free, then this would increase used_rooms
+            used_rooms += 1
+            start_pointer += 1
+
+        return used_rooms
+
+
+
+
+
+
+
+
+
+
