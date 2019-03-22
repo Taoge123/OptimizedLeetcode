594b9ed1c2fa62ba978f9ee5de9d1bc0c7e24386
taocheng <taocheng984@gmail.com>
2019-03-22 07:04:10 -0400

    LC_253_Meeting_Rooms_II.py

--- LeetcodeNew/Heap/LC_253_Meeting_Rooms_II.py
+++ LeetcodeNew/Heap/LC_253_Meeting_Rooms_II.py
@@ -122,19 +122,8 @@ class Solution1:
 
 
 
-
-# Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
-
 class Solution2:
     def minMeetingRooms(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: int
-        """
 
         # If there are no meetings, we don't need any rooms.
         if not intervals:
