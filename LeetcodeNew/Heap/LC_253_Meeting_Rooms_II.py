6c9e02ff89c538bb042e688764ddbb4f761d7300
taocheng <taocheng984@gmail.com>
2019-03-22 07:21:56 -0400

    LC_253_Meeting_Rooms_II.py

--- LeetcodeNew/Heap/LC_253_Meeting_Rooms_II.py
+++ LeetcodeNew/Heap/LC_253_Meeting_Rooms_II.py
@@ -15,7 +15,18 @@ Example 2:
 Input: [[7,10],[2,4]]
 Output: 1
 """
-
+"""
+1. Sort the given meetings by their start time.
+2. Initialize a new min-heap and add the first meeting's ending time to the heap. 
+   We simply need to keep track of the ending times as that tells us when a meeting room will get free.
+3. For every meeting room check if the minimum element of the heap i.e. 
+   the room at the top of the heap is free or not.
+4. If the room is free, then we extract the topmost element 
+   and add it back with the ending time of the current meeting we are processing.
+5. If not, then we allocate a new room and add it to the heap.
+6. After processing all the meetings, the size of the heap will tell us the number of rooms allocated. 
+   This will be the minimum number of rooms needed to accommodate all the meetings.
+"""
 # Definition for an interval.
 import heapq
 
@@ -32,6 +43,42 @@ class Interval:
  # you need to find a new room someplace else ( numRooms += 1 ).  
  # After you finish the meeting, the room becomes available again ( available += 1 ).
  """
+
+
+class Solution1:
+    def minMeetingRooms(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: int
+        """
+
+        # If there is no meeting to schedule then no room needs to be allocated.
+        if not intervals:
+            return 0
+
+        # The heap initialization
+        free_rooms = []
+
+        # Sort the meetings in increasing order of their start time.
+        intervals.sort(key= lambda x: x.start)
+
+        # Add the first meeting. We have to give a new room to the first meeting.
+        heapq.heappush(free_rooms, intervals[0].end)
+
+        # For all the remaining meeting rooms
+        for i in intervals[1:]:
+
+            # If the room due to free up the earliest is free, assign that room to this meeting.
+            if free_rooms[0] <= i.start:
+                heapq.heappop(free_rooms)
+
+            # If a new room is to be assigned, then also we add to the heap,
+            # If an old room is allocated, then also we have to add to the heap with updated end time.
+            heapq.heappush(free_rooms, i.end)
+
+        # The size of the heap tells us the minimum rooms required for all the meetings.
+        return len(free_rooms)
+
 
 class Solution11:
     def minMeetingRooms(self, intervals):
@@ -86,40 +133,6 @@ class SolutionCaikehe:
         return len(heap)
 
 
-class Solution1:
-    def minMeetingRooms(self, intervals):
-        """
-        :type intervals: List[Interval]
-        :rtype: int
-        """
-
-        # If there is no meeting to schedule then no room needs to be allocated.
-        if not intervals:
-            return 0
-
-        # The heap initialization
-        free_rooms = []
-
-        # Sort the meetings in increasing order of their start time.
-        intervals.sort(key= lambda x: x.start)
-
-        # Add the first meeting. We have to give a new room to the first meeting.
-        heapq.heappush(free_rooms, intervals[0].end)
-
-        # For all the remaining meeting rooms
-        for i in intervals[1:]:
-
-            # If the room due to free up the earliest is free, assign that room to this meeting.
-            if free_rooms[0] <= i.start:
-                heapq.heappop(free_rooms)
-
-            # If a new room is to be assigned, then also we add to the heap,
-            # If an old room is allocated, then also we have to add to the heap with updated end time.
-            heapq.heappush(free_rooms, i.end)
-
-        # The size of the heap tells us the minimum rooms required for all the meetings.
-        return len(free_rooms)
-
 
 
 class Solution2:
@@ -155,9 +168,6 @@ class Solution2:
             start_pointer += 1
 
         return used_rooms
-
-
-
 
 
 
