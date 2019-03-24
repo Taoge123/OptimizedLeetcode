d1cad266ef7eef4c17245b8c967a06d5b0a29498
taocheng <taocheng984@gmail.com>
2019-03-23 22:25:31 -0400

    LC_659_Split_Array_into_Consecutive_Subsequences.py

---
+++ LeetcodeNew/Heap/LC_659_Split_Array_into_Consecutive_Subsequences.py
@@ -0,0 +1,64 @@
+
+"""
+You are given an integer array sorted in ascending order (may contain duplicates), you need to split them into several subsequences, where each subsequences consist of at least 3 consecutive integers. Return whether you can make such a split.
+
+Example 1:
+Input: [1,2,3,3,4,5]
+Output: True
+Explanation:
+You can split them into two consecutive subsequences :
+1, 2, 3
+3, 4, 5
+Example 2:
+Input: [1,2,3,3,4,4,5,5]
+Output: True
+Explanation:
+You can split them into two consecutive subsequences :
+1, 2, 3, 4, 5
+3, 4, 5
+Example 3:
+Input: [1,2,3,4,4,5]
+Output: False
+"""
+
+"""
+I used a greedy algorithm.
+leftis a hashmap, left[i] counts the number of i that I haven't placed yet.
+endis a hashmap, end[i] counts the number of consecutive subsequences that ends at number i
+Then I tried to split the nums one by one.
+If I could neither add a number to the end of a existing consecutive subsequence 
+nor find two following number in the left, I returned False
+"""
+
+import collections
+
+class SolutionLee:
+    def isPossible(self, nums):
+        left = collections.Counter(nums)
+        end = collections.Counter()
+        for i in nums:
+            if not left[i]: continue
+            left[i] -= 1
+            if end[i - 1] > 0:
+                end[i - 1] -= 1
+                end[i] += 1
+            elif left[i + 1] and left[i + 2]:
+                left[i + 1] -= 1
+                left[i + 2] -= 1
+                end[i + 2] += 1
+            else:
+                return False
+        return True
+
+
+
+
+
+
+
+
+
+
+
+
+
