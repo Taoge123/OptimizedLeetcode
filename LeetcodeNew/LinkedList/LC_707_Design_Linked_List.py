46908db1034410dd677f897d88c30306e507b10f
taocheng <taocheng984@gmail.com>
2019-03-28 21:07:34 -0400

    LC_707_Design_Linked_List.py

---
+++ LeetcodeNew/LinkedList/LC_707_Design_Linked_List.py
@@ -0,0 +1,200 @@
+
+"""
+Design your implementation of the linked list. You can choose to use the singly linked list or the doubly linked list.
+A node in a singly linked list should have two attributes: val and next. val is the value of the current node,
+and next is a pointer/reference to the next node. If you want to use the doubly linked list,
+you will need one more attribute prev to indicate the previous node in the linked list.
+Assume all nodes in the linked list are 0-indexed.
+
+Implement these functions in your linked list class:
+
+get(index) : Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+addAtHead(val) : Add a node of value val before the first element of the linked list.
+After the insertion, the new node will be the first node of the linked list.
+addAtTail(val) : Append a node of value val to the last element of the linked list.
+addAtIndex(index, val) : Add a node of value val before the index-th node in the linked list.
+If index equals to the length of linked list, the node will be appended to the end of linked list.
+If index is greater than the length, the node will not be inserted.
+deleteAtIndex(index) : Delete the index-th node in the linked list, if the index is valid.
+Example:
+
+MyLinkedList linkedList = new MyLinkedList();
+linkedList.addAtHead(1);
+linkedList.addAtTail(3);
+linkedList.addAtIndex(1, 2);  // linked list becomes 1->2->3
+linkedList.get(1);            // returns 2
+linkedList.deleteAtIndex(1);  // now the linked list is 1->3
+linkedList.get(1);            // returns 3
+Note:
+
+All values will be in the range of [1, 1000].
+The number of operations will be in the range of [1, 1000].
+Please do not use the built-in LinkedList library.
+"""
+
+
+class Node(object):
+
+    def __init__(self, val):
+        self.val = val
+        self.next = None
+
+
+class MyLinkedList(object):
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.head = None
+        self.size = 0
+
+    def get(self, index):
+        """
+        Get the value of the index-th node in the linked list. If the index is invalid, return -1.
+        :type index: int
+        :rtype: int
+        """
+        if index < 0 or index >= self.size:
+            return -1
+
+        if self.head is None:
+            return -1
+
+        curr = self.head
+        for i in range(index):
+            curr = curr.next
+        return curr.val
+
+    def addAtHead(self, val):
+        """
+        Add a node of value val before the first element of the linked list.
+        After the insertion, the new node will be the first node of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        node = Node(val)
+        node.next = self.head
+        self.head = node
+
+        self.size += 1
+
+    def addAtTail(self, val):
+        """
+        Append a node of value val to the last element of the linked list.
+        :type val: int
+        :rtype: void
+        """
+        curr = self.head
+        if curr is None:
+            self.head = Node(val)
+        else:
+            while curr.next is not None:
+                curr = curr.next
+            curr.next = Node(val)
+
+        self.size += 1
+
+    def addAtIndex(self, index, val):
+        """
+        Add a node of value val before the index-th node in the linked list.
+        If index equals to the length of linked list, the node will be appended to the end of linked list.
+        If index is greater than the length, the node will not be inserted.
+        :type index: int
+        :type val: int
+        :rtype: void
+        """
+        if index < 0 or index > self.size:
+            return
+
+        if index == 0:
+            self.addAtHead(val)
+        else:
+            curr = self.head
+            for i in range(index - 1):
+                curr = curr.next
+            node = Node(val)
+            node.next = curr.next
+            curr.next = node
+
+            self.size += 1
+
+    def deleteAtIndex(self, index):
+        """
+        Delete the index-th node in the linked list, if the index is valid.
+        :type index: int
+        :rtype: void
+        """
+        if index < 0 or index >= self.size:
+            return
+
+        curr = self.head
+        if index == 0:
+            self.head = curr.next
+        else:
+            for i in range(index - 1):
+                curr = curr.next
+            curr.next = curr.next.next
+
+        self.size -= 1
+
+
+class MyLinkedList2:
+
+    def __init__(self):
+        self.head = self.tail = Node(-1)
+        self.head.next = self.tail
+        self.tail.pre = self.head
+        self.size = 0
+
+    def add(self, preNode, val):
+        node = Node(val)
+        node.pre = preNode
+        node.next = preNode.next
+        node.pre.next = node.next.pre = node
+        self.size += 1
+
+    def remove(self, node):
+        node.pre.next = node.next
+        node.next.pre = node.pre
+        self.size -= 1
+
+    def forward(self, start, end, cur):
+        while start != end:
+            start += 1
+            cur = cur.next
+        return cur
+
+    def backward(self, start, end, cur):
+        while start != end:
+            start -= 1
+            cur = cur.pre
+        return cur
+
+    def get(self, index):
+        if 0 <= index <= self.size // 2:
+            return self.forward(0, index, self.head.next).val
+        elif self.size // 2 < index < self.size:
+            return self.backward(self.size - 1, index, self.tail.pre).val
+        return -1
+
+    def addAtHead(self, val):
+        self.add(self.head, val)
+
+    def addAtTail(self, val):
+        self.add(self.tail.pre, val)
+
+    def addAtIndex(self, index, val):
+        if 0 <= index <= self.size // 2:
+            self.add(self.forward(0, index, self.head.next).pre, val)
+        elif self.size // 2 < index <= self.size:
+            self.add(self.backward(self.size, index, self.tail).pre, val)
+
+    def deleteAtIndex(self, index):
+        if 0 <= index <= self.size // 2:
+            self.remove(self.forward(0, index, self.head.next))
+        elif self.size // 2 < index < self.size:
+            self.remove(self.backward(self.size - 1, index, self.tail.pre))
+
+
+
