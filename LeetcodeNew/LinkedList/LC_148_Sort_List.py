86f883e46a3aa5da03e1b6a0fd712f2785cbdb75
taocheng <taocheng984@gmail.com>
2019-03-30 14:14:29 -0400

    LC_148_Sort_List.py

---
+++ LeetcodeNew/LinkedList/LC_148_Sort_List.py
@@ -0,0 +1,138 @@
+
+"""
+Sort a linked list in O(n log n) time using constant space complexity.
+
+Example 1:
+
+Input: 4->2->1->3
+Output: 1->2->3->4
+Example 2:
+
+Input: -1->5->3->4->0
+Output: -1->0->3->4->5
+"""
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution1:
+    def merge(self, h1, h2):
+        dummy = tail = ListNode(None)
+        while h1 and h2:
+            if h1.val < h2.val:
+                tail.next, h1 = h1, h1.next
+            else:
+                tail.next, h2 = h2, h2.next
+            tail = tail.next
+
+        tail.next = h1 or h2
+        return dummy.next
+
+ 
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+
+        pre, slow, fast = None, head, head
+        while fast and fast.next:
+            pre, slow, fast = slow, slow.next, fast.next.next
+        pre.next = None
+
+        return self.merge(*map(self.sortList, (head, slow)))
+
+
+class SolutionCaikehe:
+    # merge sort, recursively
+    def sortList(self, head):
+        if not head or not head.next:
+            return head
+        # divide list into two parts
+        fast, slow = head.next, head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        second = slow.next
+        # cut down the first part
+        slow.next = None
+        l = self.sortList(head)
+        r = self.sortList(second)
+        return self.merge(l, r)
+
+    # merge in-place without dummy node
+    def merge(self, l, r):
+        if not l or not r:
+            return l or r
+        if l.val > r.val:
+            l, r = r, l
+        # get the return node "head"
+        head = pre = l
+        l = l.next
+        while l and r:
+            if l.val < r.val:
+                l = l.next
+            else:
+                nxt = pre.next
+                pre.next = r
+                tmp = r.next
+                r.next = nxt
+                r = tmp
+            pre = pre.next
+        # l and r at least one is None
+        pre.next = l or r
+        return head
+
+    def merge(self, l, r):
+        if not l or not r:
+            return l or r
+        if l.val > r.val:
+            l, r = r, l
+        # get the return node "head"
+        head = pre = l
+        l = l.next
+        while l and r:
+            if l.val < r.val:
+                pre.next = l
+                l = l.next
+            else:
+                pre.next = r
+                r = r.next
+            pre = pre.next
+        # l and r at least one is None
+        pre.next = l or r
+        return head
+
+
+class Solution2:
+
+    def sortList(self, head):
+        if head is None or head.next is None:
+            return head
+        middle_node = self.find_middle_node(head)
+        right_head = middle_node.next
+        middle_node.next = None
+        return self.merge(self.sortList(head), self.sortList(right_head))
+
+    def find_middle_node(self, head):
+        slow, fast = head, head
+        while fast and fast.next and fast.next.next:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
+
+    def merge(self, head1, head2):
+        dummy = ListNode(None)
+        node = dummy
+        while head1 and head2:
+            if head1.val < head2.val:
+                node.next = head1
+                head1 = head1.next
+            else:
+                node.next = head2
+                head2 = head2.next
+            node = node.next
+
+        node.next = head1 or head2
+        return dummy.next
+
