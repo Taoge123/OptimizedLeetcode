2759149ef2748f97b1c8c244fbe789e7e5228fed
taocheng <taocheng984@gmail.com>
2019-03-31 16:45:51 -0400

    LC_125_Valid_Palindrome.py

---
+++ LeetcodeNew/TwoPointers/LC_125_Valid_Palindrome.py
@@ -0,0 +1,39 @@
+
+"""
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+Note: For the purpose of this problem, we define empty string as valid palindrome.
+
+Example 1:
+
+Input: "A man, a plan, a canal: Panama"
+Output: true
+Example 2:
+
+Input: "race a car"
+Output: false
+
+"""
+
+class Solution1:
+    def isPalindrome(self, s):
+        l, r = 0, len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True
+
+
+
+
+
+
+
+
+
