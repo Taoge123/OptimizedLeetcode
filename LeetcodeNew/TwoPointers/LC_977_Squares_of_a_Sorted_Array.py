df6950b4d2cd07cb8af8925717ac6fd566e79143
taocheng <taocheng984@gmail.com>
2019-03-31 14:13:04 -0400

    LC_977_Squares_of_a_Sorted_Array.py

---
+++ LeetcodeNew/TwoPointers/LC_977_Squares_of_a_Sorted_Array.py
@@ -0,0 +1,96 @@
+
+"""
+Given an array of integers A sorted in non-decreasing order,
+return an array of the squares of each number, also in sorted non-decreasing order.
+
+ Example 1:
+
+Input: [-4,-1,0,3,10]
+Output: [0,1,9,16,100]
+Example 2:
+
+Input: [-7,-3,2,3,11]
+Output: [4,9,9,49,121]
+"""
+import collections
+
+class Solution11:
+    def sortedSquares(self, A):
+        answer = collections.deque()
+        l, r = 0, len(A) - 1
+        while l <= r:
+            left, right = abs(A[l]), abs(A[r])
+            if left > right:
+                answer.appendleft(left * left)
+                l += 1
+            else:
+                answer.appendleft(right * right)
+                r -= 1
+        return list(answer)
+
+
+class Solution22:
+    def sortedSquares(self, A):
+        answer = [0] * len(A)
+        l, r = 0, len(A) - 1
+        while l <= r:
+            left, right = abs(A[l]), abs(A[r])
+            if left > right:
+                answer[r - l] = left * left
+                l += 1
+            else:
+                answer[r - l] = right * right
+                r -= 1
+        return answer
+
+
+class Solution33:
+    def sortedSquares(self, A):
+        answer = []  # list
+        l, r = 0, len(A) - 1
+        while l <= r:
+            left, right = abs(A[l]), abs(A[r])
+            if left > right:
+                answer.append(left * left)
+                l += 1
+            else:
+                answer.append(right * right)
+                r -= 1
+        return (answer[::-1])  # reverse list using step of -1 [::-1]
+
+
+class Solution1:
+    def sortedSquares(self, A):
+        return sorted(x*x for x in A)
+
+class Solution2:
+    def sortedSquares(self, A):
+        N = len(A)
+        # i, j: negative, positive parts
+        j = 0
+        while j < N and A[j] < 0:
+            j += 1
+        i = j - 1
+
+        ans = []
+        while 0 <= i and j < N:
+            if A[i]**2 < A[j]**2:
+                ans.append(A[i]**2)
+                i -= 1
+            else:
+                ans.append(A[j]**2)
+                j += 1
+
+        while i >= 0:
+            ans.append(A[i]**2)
+            i -= 1
+        while j < N:
+            ans.append(A[j]**2)
+            j += 1
+
+        return ans
+
+
+
+
+
