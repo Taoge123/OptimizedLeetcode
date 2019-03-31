5eb7f1289c85115f4804f58bfd4f8608314d1a9b
taocheng <taocheng984@gmail.com>
2019-03-31 12:45:50 -0400

    LC_082_Remove_Duplicates_from_Sorted_List_II.py

---
+++ LeetcodeNew/LinkedList/LC_082_Remove_Duplicates_from_Sorted_List_II.py
@@ -0,0 +1,64 @@
+
+"""
+Given a sorted linked list, delete all nodes that have duplicate numbers,
+leaving only distinct numbers from the original list.
+
+Example 1:
+
+Input: 1->2->3->3->4->4->5
+Output: 1->2->5
+Example 2:
+
+Input: 1->1->1->2->3
+Output: 2->3
+"""
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def deleteDuplicates(self, head):
+
+        dummy = ListNode(0);  # construct a dummy node
+        dummy.next = head
+
+        pre = dummy  # set up pre and cur pointers
+        cur = head
+        while cur:
+            if cur.next and cur.val == cur.next.val:
+                # loop until cur point to the last duplicates
+                while cur and cur.next and cur.val == cur.next.val:
+                    cur = cur.next
+                pre.next = cur.next  # propose the next for pre
+                # this will be verified by next line
+            else:
+                pre = pre.next
+            cur = cur.next
+        return dummy.next
+
+
+
+class SolutionCaikehe:
+    def deleteDuplicates(self, head):
+        dummy = pre = ListNode(0)
+        dummy.next = head
+        while head and head.next:
+            if head.val == head.next.val:
+                while head and head.next and head.val == head.next.val:
+                    head = head.next
+                head = head.next
+                pre.next = head
+            else:
+                pre = pre.next
+                head = head.next
+        return dummy.next
+
+
+
+
+
+
+
+
