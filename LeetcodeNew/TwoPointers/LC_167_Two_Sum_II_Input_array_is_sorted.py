6ad7e8e6c67461ebca4062ea205847ac853b3557
taocheng <taocheng984@gmail.com>
2019-03-31 15:09:33 -0400

    LC_167_Two_Sum_II_Input_array_is_sorted.py

---
+++ LeetcodeNew/TwoPointers/LC_167_Two_Sum_II_Input_array_is_sorted.py
@@ -0,0 +1,66 @@
+
+"""
+Given an array of integers that is already sorted in ascending order,
+find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers
+such that they add up to the target, where index1 must be less than index2.
+
+Note:
+
+Your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+Example:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+"""
+
+class SolutionCaikege:
+    # two-pointer
+    def twoSum1(self, numbers, target):
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return [l + 1, r + 1]
+            elif s < target:
+                l += 1
+            else:
+                r -= 1
+
+    # dictionary
+    def twoSum2(self, numbers, target):
+        dic = {}
+        for i, num in enumerate(numbers):
+            if target - num in dic:
+                return [dic[target - num] + 1, i + 1]
+            dic[num] = i
+
+    # binary search
+    def twoSum(self, numbers, target):
+        for i in xrange(len(numbers)):
+            l, r = i + 1, len(numbers) - 1
+            tmp = target - numbers[i]
+            while l <= r:
+                mid = l + (r - l) // 2
+                if numbers[mid] == tmp:
+                    return [i + 1, mid + 1]
+                elif numbers[mid] < tmp:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+
+
+
+
+
+
+
+
+
+
+
+
+
