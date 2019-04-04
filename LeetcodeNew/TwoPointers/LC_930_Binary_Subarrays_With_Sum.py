55d9f186560907aaaa8733cc8efc79d53c3a2be0
taocheng <taocheng984@gmail.com>
2019-04-03 21:25:55 -0400

    LC_930_Binary_Subarrays_With_Sum.py

--- LeetcodeNew/TwoPointers/LC_930_Binary_Subarrays_With_Sum.py
+++ LeetcodeNew/TwoPointers/LC_930_Binary_Subarrays_With_Sum.py
@@ -31,6 +31,26 @@ to improve the run time.
 """
 import collections
 
+class SolutionOfficial:
+    def numSubarraysWithSum(self, A, S):
+        indexes = [-1] + [ix for ix, v in enumerate(A) if v] + [len(A)]
+        ans = 0
+
+        if S == 0:
+            for i in range(len(indexes) - 1):
+                # w: number of zeros between two consecutive ones
+                w = indexes[i+1] - indexes[i] - 1
+                ans += w * (w+1) / 2
+            return ans
+
+        for i in range(1, len(indexes) - S):
+            j = i + S - 1
+            left = indexes[i] - indexes[i-1]
+            right = indexes[j+1] - indexes[j]
+            ans += left * right
+        return ans
+
+
 class SolutionLee:
     def numSubarraysWithSum(self, A, S):
         c = collections.Counter({0: 1})
