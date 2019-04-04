c042eaa12fef4be969b67ceafc33adde2a592039
taocheng <taocheng984@gmail.com>
2019-04-03 21:25:18 -0400

    LC_086_Partition_List.py

---
+++ LeetcodeNew/TwoPointers/LC_086_Partition_List.py
@@ -0,0 +1,85 @@
+
+"""
+Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+
+You should preserve the original relative order of the nodes in each of the two partitions.
+
+Example:
+
+Input: head = 1->4->3->2->5->2, x = 3
+Output: 1->2->2->4->3->5
+
+"""
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution1:
+    def partition(self, head, x):
+        # before and after are the two pointers used to create two list
+        # before_head and after_head are used to save the heads of the two lists.
+        # All of these are initialized with the dummy nodes created.
+        before = before_head = ListNode(0)
+        after = after_head = ListNode(0)
+
+        while head:
+            # If the original list node is lesser than the given x,
+            # assign it to the before list.
+            if head.val < x:
+                before.next = head
+                before = before.next
+            else:
+                # If the original list node is greater or equal to the given x,
+                # assign it to the after list.
+                after.next = head
+                after = after.next
+
+            # move ahead in the original list
+            head = head.next
+
+        # Last node of "after" list would also be ending node of the reformed list
+        after.next = None
+        # Once all the nodes are correctly assigned to the two lists,
+        # combine them to form a single list which would be returned.
+        before.next = after_head.next
+
+        return before_head.next
+
+
+class Solution2:
+    def partition(self, head, x):
+        small = ks = ListNode(0)
+        great = kg = ListNode(0)
+
+        while head:
+            if head.val < x:
+                small.next = ListNode(head.val)
+                small = small.next
+            else:
+                great.next = ListNode(head.val)
+                great = great.next
+            head = head.next
+
+        small.next = kg.next  # where I think can be much easier
+        return ks.next
+
+class SolutionCaikehe:
+    def partition(self, head, x):
+        h1 = l1 = ListNode(0)
+        h2 = l2 = ListNode(0)
+        while head:
+            if head.val < x:
+                l1.next = head
+                l1 = l1.next
+            else:
+                l2.next = head
+                l2 = l2.next
+            head = head.next
+        l2.next = None
+        l1.next = h2.next
+        return h1.next
+
+
+
