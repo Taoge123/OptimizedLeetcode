3330b21b9f7867b939eace75dccc458a3b71887f
taocheng <taocheng984@gmail.com>
2019-04-03 21:00:42 -0400

    LC_080_Remove_Duplicates_from_Sorted_Array_II.py

---
+++ LeetcodeNew/TwoPointers/LC_080_Remove_Duplicates_from_Sorted_Array_II.py
@@ -0,0 +1,67 @@
+
+"""
+Given a sorted array nums, remove the duplicates in-place such that duplicates appeared at most twice and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,1,2,2,3],
+
+Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,1,2,3,3],
+
+Your function should return length = 7, with the first seven elements of nums being modified to 0, 0, 1, 1, 2, 3 and 3 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+Clarification:
+
+Confused why the returned value is an integer but your answer is an array?
+
+Note that the input array is passed in by reference, which means modification to the input array will be known to the caller as well.
+
+Internally you can think of this:
+
+// nums is passed in by reference. (i.e., without making a copy)
+int len = removeDuplicates(nums);
+
+// any modification to nums in your function would be known by the caller.
+// using the length returned by your function, it prints the first len elements.
+for (int i = 0; i < len; i++) {
+    print(nums[i]);
+}
+"""
+
+class Solution1:
+    def removeDuplicates(self, nums):
+        tail = 0
+        for num in nums:
+            if tail < 2 or num != nums[tail - 1] or num != nums[tail - 2]:
+                nums[tail] = num
+                tail += 1
+        return tail
+
+
+class Solution2:
+    def removeDuplicates(self, nums):
+        for i in reversed(range(len(nums))):
+            if i < 2:
+                break
+            if nums[i] == nums[i-1] and nums[i] == nums[i-2]:
+                nums.pop(i)
+        return(len(nums))
+
+class Solution3:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        idx = 0
+        for i in range(0, len(nums)):
+            if idx < 2 or nums[i] != nums[idx - 2]:
+                nums[idx] = nums[i]
+                idx += 1
+        return idx
+
+
