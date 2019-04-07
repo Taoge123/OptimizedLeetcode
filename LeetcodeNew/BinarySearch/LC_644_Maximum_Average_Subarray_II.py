443983f03e33d22d6e3dfe8926e8a50cc64464fa
taocheng <taocheng984@gmail.com>
2019-04-07 16:05:39 -0400

    LC_644_Maximum_Average_Subarray_II.py

---
+++ LeetcodeNew/BinarySearch/LC_644_Maximum_Average_Subarray_II.py
@@ -0,0 +1,37 @@
+
+"""
+https://leetcode.com/problems/maximum-average-subarray-ii/discuss/105476/Python-Advanced-O(n)-solution-(Convex-Hull-Window)
+https://leetcode.com/problems/maximum-average-subarray-ii/discuss/105482/Python-solution-with-detailed-explanation
+https://leetcode.com/problems/maximum-average-subarray-ii/discuss/105489/Python-Straightforward-with-Explanation
+
+
+Given an array consisting of n integers, find the contiguous subarray whose length is greater than
+or equal to k that has the maximum average value. And you need to output the maximum average value.
+
+Example 1:
+Input: [1,12,-5,-6,50,3], k = 4
+Output: 12.75
+Explanation:
+when length is 5, maximum average value is 10.8,
+when length is 6, maximum average value is 9.16667.
+Thus return 12.75.
+Note:
+1 <= k <= n <= 10,000.
+Elements of the given array will be in range [-10,000, 10,000].
+The answer with the calculation error less than 10-5 will be accepted.
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
