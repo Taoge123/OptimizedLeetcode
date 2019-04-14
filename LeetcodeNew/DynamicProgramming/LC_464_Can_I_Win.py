e5b5f7dcea151a578320973c6207ba8c90b509ac
taocheng <taocheng984@gmail.com>
2019-04-13 21:34:21 -0400

    LC_464_Can_I_Win.py

--- LeetcodeNew/DynamicProgramming/LC_464_Can_I_Win.py
+++ LeetcodeNew/DynamicProgramming/LC_464_Can_I_Win.py
@@ -426,6 +426,28 @@ class Solution6:
 
 
 
+class SolutionHuahua:
+  def canIWin(self, M, T):
+    def win(M, T, m, state):
+      if T <= 0: return False
+      if m[state] != 0: return m[state] == 1
+      for i in range(M):
+        #number i used
+        if (state & (1 << i)) > 0: continue
+        #The next player can not win, current player wins
+        if not win(M, T - i - 1, m, state | (1 << i)):
+          m[state] = 1
+          return True
+      m[state] = -1
+      return False
+
+    s = M * (M + 1) / 2
+    if s < T: return False
+    if T <= 0: return True
+    if s == T: return (M % 2) == 1
+
+    m = [0] * (1 << M)
+    return win(M, T, m, 0)
 
 
 
