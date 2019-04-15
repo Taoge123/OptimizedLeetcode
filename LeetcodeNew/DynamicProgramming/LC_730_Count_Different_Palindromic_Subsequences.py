d71dd3956223a0466b5f3e4f43795f9abde5d178
taocheng <taocheng984@gmail.com>
2019-04-15 19:57:35 -0400

    LC_730_Count_Different_Palindromic_Subsequences.py

--- LeetcodeNew/DynamicProgramming/LC_730_Count_Different_Palindromic_Subsequences.py
+++ LeetcodeNew/DynamicProgramming/LC_730_Count_Different_Palindromic_Subsequences.py
@@ -7,7 +7,7 @@ Palindromic Substrings
 https://leetcode.com/problems/count-different-palindromic-subsequences/discuss/109510/Python-DP%2BDFS-O(n2)-with-Explanations
 http://www.cnblogs.com/grandyang/p/7942040.html
 https://zxi.mytechroad.com/blog/dynamic-programming/leetcode-730-count-different-palindromic-subsequences/
-
+https://leetcode.com/problems/count-different-palindromic-subsequences/solution/
 
 
 Given a string S, find the number of different non-empty palindromic subsequences in S,
@@ -320,6 +320,78 @@ public:
 
 """
 
+class Solution2:
+    def countPalindromicSubsequences(self, S):
+        N = len(S)
+        A = [ord(c) - ord('a') for c in S]
+        prv = [None] * N
+        nxt = [None] * N
+
+        last = [None] * 4
+        for i in xrange(N):
+            last[A[i]] = i
+            prv[i] = tuple(last)
+
+        last = [None] * 4
+        for i in xrange(N-1, -1, -1):
+            last[A[i]] = i
+            nxt[i] = tuple(last)
+
+        MOD = 10**9 + 7
+        memo = [[None] * N for _ in xrange(N)]
+        def dp(i, j):
+            if memo[i][j] is not None:
+                return memo[i][j]
+            ans = 1
+            if i <= j:
+                for x in xrange(4):
+                    i0 = nxt[i][x]
+                    j0 = prv[j][x]
+                    if i <= i0 <= j:
+                        ans += 1
+                    if None < i0 < j0:
+                        ans += dp(i0+1, j0-1)
+            ans %= MOD
+            memo[i][j] = ans
+            return ans
+
+        return dp(0, N-1) - 1
+
+class Solution3:
+    def countPalindromicSubsequences(self, S):
+        n = len(S)
+        mod = 1000000007
+        dp = [[[0 for _ in range(n)] for _ in range(n)] for _ in range(4)]
+
+        for i in range(n - 1, -1, -1):
+            for j in range(i, n):
+                for k in range(4):
+                    c = chr(ord('a') + k)
+                    if j == i:
+                        if S[i] == c:
+                            dp[k][i][j] = 1
+                        else:
+                            dp[k][i][j] = 0
+                    else:  # j > i
+                        if S[i] != c:
+                            dp[k][i][j] = dp[k][i + 1][j]
+                        elif S[j] != c:
+                            dp[k][i][j] = dp[k][i][j - 1]
+                        else:  # S[i] == S[j] == c
+                            if j == i + 1:
+                                dp[k][i][j] = 2  # "aa" : {"a", "aa"}
+                            else:  # length is > 2
+                                dp[k][i][j] = 2
+                                for m in range(4):  # count each one within subwindows [i+1][j-1]
+                                    dp[k][i][j] += dp[m][i + 1][j - 1]
+                                    dp[k][i][j] %= mod
+
+        ans = 0
+        for k in range(4):
+            ans += dp[k][0][n - 1]
+            ans %= mod
+
+        return ans
 
 
 
