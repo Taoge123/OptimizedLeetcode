d58e3d8ebf51a78a391854db8d0aecab8889471f
taocheng <taocheng984@gmail.com>
2019-06-03 21:24:53 -0400

    LC_297.py

---
+++ LeetcodeNew/LinkedIn/LC_297.py
@@ -0,0 +1,13 @@
+class Codec:
+    def serialize(self, root):
+        if not root: return ""
+        q = collections.deque([root])
+        res = []
+        while q:
+            node = q.popleft()
+            if node:
+                q.append(node.left)
+                q.append(node.right)
+            res.append(str(node.val) if node else '#')
+        return ','.join(res)
+
