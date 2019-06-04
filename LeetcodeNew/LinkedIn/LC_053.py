6c22e9f858cec7c0f5d47791b4624512e9f66241
taocheng <taocheng984@gmail.com>
2019-06-03 21:23:07 -0400

    LC_053.py

---
+++ LeetcodeNew/LinkedIn/LC_053.py
@@ -0,0 +1,21 @@
+# DP, O(n) space
+def maxSubArray(self, nums):
+    if not nums:
+        return None
+    dp = [0] * len(nums)
+    res = dp[0] = nums[0]
+    for i in xrange(1, len(nums)):
+        dp[i] = max(dp[i - 1] + nums[i], nums[i])
+        res = max(res, dp[i])
+    return res
+
+
+# DP, constant space
+def maxSubArray2(self, nums):
+    if not nums:
+        return None
+    loc = glo = nums[0]
+    for i in xrange(1, len(nums)):
+        loc = max(loc + nums[i], nums[i])
+        glo = max(loc, glo)
+    return glo
