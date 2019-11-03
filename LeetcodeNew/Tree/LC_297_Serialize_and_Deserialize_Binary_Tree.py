54c39b2ab5f84a37cb7fbb2311a66d6b88145aa8
taocheng <taocheng984@gmail.com>
2019-11-02 22:19:58 -0400

    LC_297_Serialize_and_Deserialize_Binary_Tree.py

--- LeetcodeNew/Tree/LC_297_Serialize_and_Deserialize_Binary_Tree.py
+++ LeetcodeNew/Tree/LC_297_Serialize_and_Deserialize_Binary_Tree.py
@@ -30,63 +30,51 @@ You may serialize the following tree:
 as "[1,2,3,null,null,4,5]"
 
 """
-
+import collections
 
 class TreeNode:
-    """ Definition of a binary tree node."""
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
 
-# Serialization
 class Codec:
 
     def serialize(self, root):
-        """ Encodes a tree to a single string.
-        :type root: TreeNode
-        :rtype: str
-        """
+        if not root:
+            return ""
 
-        def rserialize(root, string):
-            """ a recursive helper function for the serialize() function."""
-            # check base case
-            if root is None:
-                string += 'None,'
-            else:
-                string += str(root.val) + ','
-                string = rserialize(root.left, string)
-                string = rserialize(root.right, string)
-            return string
-
-        return rserialize(root, '')
-
-
-# Deserialization
-class Codec2:
+        queue = collections.deque([root])
+        res = []
+        while queue:
+            node = queue.popleft()
+            if node:
+                queue.append(node.left)
+                queue.append(node.right)
+            res.append(str(node.val) if node else "#")
+        return ",".join(res)
 
     def deserialize(self, data):
-        """Decodes your encoded data to tree.
-        :type data: str
-        :rtype: TreeNode
-        """
-
-        def rdeserialize(l):
-            """ a recursive helper function for deserialization."""
-            if l[0] == 'None':
-                l.pop(0)
-                return None
-
-            root = TreeNode(l[0])
-            l.pop(0)
-            root.left = rdeserialize(l)
-            root.right = rdeserialize(l)
-            return root
-
-        data_list = data.split(',')
-        root = rdeserialize(data_list)
+        if not data:
+            return None
+        nodes = data.split(",")
+        root = TreeNode(int(nodes[0]))
+        queue = collections.deque([root])
+        index = 1
+        while queue:
+            node = queue.popleft()
+            if nodes[index] is not "#":
+                node.left = TreeNode(int(nodes[index]))
+                queue.append(node.left)
+            index += 1
+            if nodes[index] is not "#":
+                node.right = TreeNode(int(nodes[index]))
+                queue.append(node.right)
+            index += 1
         return root
+
+
 
 
 
