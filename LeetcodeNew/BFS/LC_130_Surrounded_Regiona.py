b7563582fe250ffb147ff753bfc719178caae2b8
taocheng <taocheng984@gmail.com>
2019-11-03 13:21:47 -0500

    130

--- LeetcodeNew/BFS/LC_130_Surrounded_Regiona.py
+++ LeetcodeNew/BFS/LC_130_Surrounded_Regiona.py
@@ -28,65 +28,35 @@ if they are adjacent cells connected horizontally or vertically.
 import collections
 
 
-class SolutionBFS:
-    # BFS
-    def solve(self, board):
-        queue = collections.deque([])
-        for r in range(len(board)):
-            for c in range(len(board[0])):
-                if (r in [0, len(board) - 1] or c in [0, len(board[0]) - 1]) and board[r][c] == "O":
-                    queue.append((r, c))
-        while queue:
-            r, c = queue.popleft()
-            if 0 <= r < len(board) and 0 <= c < len(board[0]) and board[r][c] == "O":
-                board[r][c] = "D"
-                queue.append((r - 1, c))
-                queue.append((r + 1, c))
-                queue.append((r, c - 1))
-                queue.append((r, c + 1))
-
-        for r in range(len(board)):
-            for c in range(len(board[0])):
-                if board[r][c] == "O":
-                    board[r][c] = "X"
-                elif board[r][c] == "D":
-                    board[r][c] = "O"
-
-
-
-
-
-class SolutionDFS:
+class Solution:
     def solve(self, board):
         if len(board) == 0:
             return
-        height = len(board)
-        width = len(board[0])
-        def dfs(board, y, x):
-            if x < 0 or x >= width or y < 0 or y >= height or board[y][x] != 'O':
-                return
-            board[y][x] = 'S'
-            dfs(board, y + 1, x)
-            dfs(board, y - 1, x)
-            dfs(board, y, x + 1)
-            dfs(board, y, x - 1)
-        for i in range(width):
-            if board[0][i] == 'O':
-                dfs(board, 0, i)
-            if board[height-1][i] == 'O':
-                dfs(board, height-1, i)
-        for j in range(height):
-            if board[j][0] == 'O':
-                dfs(board, j, 0)
-            if board[j][width-1] == 'O':
-                dfs(board, j, width-1)
-        for y in range(height):
-            for x in range(width):
-                if board[y][x] == 'S':
-                    board[y][x] = 'O'
-                else:
-                    board[y][x] = 'X'
+        m, n = len(board), len(board[0])
+        self.directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
+        for i in range(m):
+            self.dfs(i, 0, board, m, n)
+            self.dfs(i, n - 1, board, m, n)
+
+        for j in range(1, n - 1):
+            self.dfs(0, j, board, m, n)
+            self.dfs(m - 1, j, board, m, n)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == 'D':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'
+
+    def dfs(self, i, j, board, m, n):
+        if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != 'O':
+            return
+        board[i][j] = 'D'
+        for direction in self.directions:
+            x, y = i + direction[0], j + direction[1]
+            self.dfs(x, y, board, m, n)
 
 
 
