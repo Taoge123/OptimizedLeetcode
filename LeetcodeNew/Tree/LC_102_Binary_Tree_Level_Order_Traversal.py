a5b4957322151423965256508a96ca4050d0fea8
taocheng <taocheng984@gmail.com>
2019-11-02 21:09:07 -0400

    LC_102_Binary_Tree_Level_Order_Traversal.py

--- LeetcodeNew/Tree/LC_102_Binary_Tree_Level_Order_Traversal.py
+++ LeetcodeNew/Tree/LC_102_Binary_Tree_Level_Order_Traversal.py
@@ -31,131 +31,40 @@ queue的概念用deque来实现，popleft() 时间复杂为O(1)即可
 for loop终止后，就可以把记录好的整层的数值，放入我们的return数组里。
 """
 
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 from collections import deque
 
 
 class Solution:
-    def levelOrder(self, root):
-        if not root: return []
-        queue, res = deque([root]), []
+    def levelOrder(self, root: TreeNode):
 
+        if not root:
+            return []
+        queue = deque([root])
+        res = []
         while queue:
-            #Size if before adding children nodes
-            cur_level, size = [], len(queue)
+
+            size, cur_level = len(queue), []
             for i in range(size):
                 node = queue.popleft()
+                cur_level.append(node.val)
+
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-                cur_level.append(node.val)
-            #res will add current value for size defined above
+
             res.append(cur_level)
+
         return res
 
 
-"""
-level is a list of the nodes in the current level.
-Keep appending a list of the values of these nodes to ans
-and then updating level with all the nodes in the next level (kids)
-until it reaches an empty level. Python's list comprehension makes it easier
-to deal with many conditions in a concise manner.
-"""
-#Iterative
-class Solution2:
-    def levelOrder(self, root):
-        if not root:
-            return []
-        ans, level = [], [root]
-        while level:
-            ans.append([node.val for node in level])
-            temp = []
-            for node in level:
-                temp.extend([node.left, node.right])
-            level = [leaf for leaf in temp if leaf]
-        return ans
-
-
-
-#Recursive
-class Solution:
-    def __init__(self):
-        self.l=[]
-    def helper(self,root,level):
-        if not root:
-            return None
-        else:
-            if level<len(self.l):
-                self.l[level].append(root.val)
-            else:
-                self.l.append([root.val])
-            self.helper(root.left,level+1)
-            self.helper(root.right,level+1)
-        return self.l
-    def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        if not root:
-            return []
-        return self.helper(root,0)
-
-
-"""
-Breadh First Search
-
-Using BFS, at any instant only L1 and L1+1 nodes are in the queue.
-When we start the while loop, we have L1 nodes in the queue.
-for _ in range(len(q)) allows us to dequeue L1 nodes one by one and add L2 children one by one.
-Time complexity: O(N). Space Complexity: O(N)
-"""
-from collections import deque
-class Solution4:
-    def levelOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        q, result = deque(), []
-        if root:
-            q.append(root)
-        while len(q):
-            level = []
-            for _ in range(len(q)):
-                x = q.popleft()
-                level.append(x.val)
-                if x.left:
-                    q.append(x.left)
-                if x.right:
-                    q.append(x.right)
-            result.append(level)
-        return result
-
-
-"""
-Depth First Search
-Use a variable to track level in the tree and use simple Pre-Order traversal
-Add sub-lists to result as we move down the levels
-Time Complexity: O(N)
-Space Complexity: O(N) + O(h) for stack space
-"""
-
-
-class Solution5:
-    def levelOrder(self, root):
-        result = []
-        self.helper(root, 0, result)
-        return result
-
-    def helper(self, root, level, result):
-        if root is None:
-            return
-        if len(result) <= level:
-            result.append([])
-        result[level].append(root.val)
-        self.helper(root.left, level + 1, result)
-        self.helper(root.right, level + 1, result)
 
 
 
