3d00eb448b95b26c3cb09661001be3eeb02faa25
taocheng <taocheng984@gmail.com>
2019-11-02 22:13:05 -0400

    LC_230_Kth_Smallest_Element_in_a_BST.py

--- LeetcodeNew/Tree/LC_230_Kth_Smallest_Element_in_a_BST.py
+++ LeetcodeNew/Tree/LC_230_Kth_Smallest_Element_in_a_BST.py
@@ -60,60 +60,27 @@ Output: 3
 """
 
 
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 class Solution:
-    def kthSmallest(self, root, k):
+    def kthSmallest(self, root: TreeNode, k: int) -> int:
         self.k = k
-        self.res = None
+        self.res = 0
         self.helper(root)
         return self.res
 
-    def helper(self, node):
-        if not node:
+    def helper(self, root):
+        if not root:
             return
-        self.helper(node.left)
+        self.helper(root.left)
         self.k -= 1
         if self.k == 0:
-            self.res = node.val
+            self.res = root.val
             return
-        self.helper(node.right)
-
-
-
-class Solution2:
-    def kthSmallest(root, k):
-        stack = []
-        while root or stack:
-            while root:
-                stack.append(root)
-                root = root.left
-            root = stack.pop()
-            k -= 1
-            if k == 0:
-                return root.val
-            root = root.right
-
-
-# The idea here is checking nodes by in order traversal,
-# if we checked k "small" nodes, the current node then will be kth smallest one:
-class Solution2:
-    def kthSmallest(self, root, k):
-        # stack records the node whether visited or not
-        stack = [(root, False)]
-        while stack:
-            curr, visited = stack.pop()
-            if curr:
-                if visited:
-                    # if visited is True, it means a "small" node is found
-                    k -= 1
-                    # if k == 0, it means k small nodes has been checked,
-                    # the current node is the kth one
-                    if k == 0:
-                        return curr.val
-                else:
-                    # Add from right to left
-                    stack.append((curr.right, False))
-                    stack.append((curr, True))
-                    stack.append((curr.left, False))
-
+        self.helper(root.right)
 
 
