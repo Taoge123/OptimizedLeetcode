b7e5dac074bb0182e316d316c49bf9e32801cef1
taocheng <taocheng984@gmail.com>
2019-11-02 22:24:06 -0400

    LC_404_Sum_of_Left_Leaves.py

--- LeetcodeNew/Tree/LC_404_Sum_of_Left_Leaves.py
+++ LeetcodeNew/Tree/LC_404_Sum_of_Left_Leaves.py
@@ -12,34 +12,27 @@ There are two left leaves in the binary tree, with values 9 and 15 respectively.
 
 """
 
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 class Solution:
-    def sumOfLeftLeaves(self, root):
-        if not root: return 0
-        if root.left and not root.left.left and not root.left.right:
-            return root.left.val + self.sumOfLeftLeaves(root.right)
-        return self.sumOfLeftLeaves(root.left) + self.sumOfLeftLeaves(root.right)   # isn't leave
+    def sumOfLeftLeaves(self, root: TreeNode) -> int:
+        self.res = 0
+        self.helper(root)
+        return self.res
 
-
-class Solution2:
-    def sumOfLeftLeaves(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        self.ans = 0
-        self.findleftleaves(root)
-        return self.ans
-
-    def findleftleaves(self, root):
+    def helper(self, root):
         if not root:
             return
-        if root.left and self.isLeaves(root.left):
-            self.ans += root.left.val
-        self.findleftleaves(root.left)
-        self.findleftleaves(root.right)
-        return
 
-    def isLeaves(self, root):
-        return root.left == None and root.right == None
+        if root.left and not root.left.left and not root.left.right:
+            self.res += root.left.val
+
+        self.helper(root.left)
+        self.helper(root.right)
+
 
 
