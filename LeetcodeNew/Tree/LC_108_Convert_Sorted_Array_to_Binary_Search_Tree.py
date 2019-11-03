4a9b87ad763695103298b44eec9bb2cfb72771c7
taocheng <taocheng984@gmail.com>
2019-11-02 21:19:10 -0400

    LC_108_Convert_Sorted_Array_to_Binary_Search_Tree.py

--- LeetcodeNew/Tree/LC_108_Convert_Sorted_Array_to_Binary_Search_Tree.py
+++ LeetcodeNew/Tree/LC_108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -31,32 +31,20 @@ class TreeNode:
         self.left = None
         self.right = None
 
+
 class Solution:
-    def sortedArrayToBST(self, nums):
-        if not nums: return None
-        l, r = 0, len(nums) - 1
-        mid = (l + (r - l)) // 2
-        node = TreeNode(nums[mid])
-        node.left = self.sortedArrayToBST(nums[:mid])
-        node.right = self.sortedArrayToBST(nums[mid+1:])
-        return node
+    def sortedArrayToBST(self, nums) -> TreeNode:
+        if not nums:
+            return None
 
+        left = 0
+        right = len(nums)
+        mid = (right - left) // 2 + left
+        root = TreeNode(nums[mid])
+        root.left = self.sortedArrayToBST(nums[:mid])
+        root.right = self.sortedArrayToBST(nums[mid + 1:])
 
-class SolutionBetter:
-    def sortedArrayToBST(self, nums):
-        self.nums = nums
-        return self.dfs(0, len(nums) - 1)
-
-    def dfs(self, l, r):
-        if l > r: return None
-        mid = (l + r) // 2
-        node = TreeNode(self.nums[mid])
-        node.left = self.dfs(l, mid - 1)
-        node.right = self.dfs(mid + 1, r)
-        return node
-
-
-
+        return root
 
 
 
