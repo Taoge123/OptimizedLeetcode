6c5b7c0dc75449733e14de857d5bae70a5dd34d7
taocheng <taocheng984@gmail.com>
2019-11-02 22:37:23 -0400

    LC_536_Construct_Binary_Tree_from_String.py

--- LeetcodeNew/Tree/LC_536_Construct_Binary_Tree_from_String.py
+++ LeetcodeNew/Tree/LC_536_Construct_Binary_Tree_from_String.py
@@ -46,33 +46,6 @@ which might be empty if we are in case #3.
 当最开始找到第一个左括号的位置时，将start赋值为该位置，那么当cnt为0时，如果start还是原来的位置，
 说明这个是左子树，我们对其调用递归函数，注意此时更新start的位置，这样就能区分左右子树了
 
-"""
-
-class TreeNode:
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-class Solution:
-    def str2tree(self, S):
-        ix = S.find('(')
-        if ix < 0:
-            return TreeNode(int(S)) if S else None
-
-        bal = 0
-        for jx, u in enumerate(S):
-            if u == '(': bal += 1
-            if u == ')': bal -= 1
-            if jx > ix and bal == 0:
-                break
-
-        root = TreeNode(int(S[:ix]))
-        root.left = self.str2tree(S[ix + 1:jx])
-        root.right = self.str2tree(S[jx + 2:-1])
-        return root
-
-"""
 下面这种解法使用迭代来做的，借助栈stack来实现。遍历字符串s，用变量j记录当前位置i，
 然后看当前遍历到的字符是什么，如果遇到的是左括号，什么也不做继续遍历；如果遇到的是数字或者负号，
 那么我们将连续的数字都找出来，然后转为整型并新建结点，此时我们看stack中是否有结点，如果有的话，
@@ -81,54 +54,43 @@ class Solution:
 
 """
 
-class Solution2:
-    def str2tree(self, s):
-        stack, cur = [], ""
-        for i, c in enumerate(s):
-            if c.isnumeric() or c == "-":
-                cur += c
-            elif not cur:
-                if c == ")":
-                    stack.pop()
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+# https://leetcode.com/problems/construct-binary-tree-from-string/discuss/100422/Python-Straightforward-with-Explanation
+# https://leetcode.com/problems/construct-binary-tree-from-string/discuss/300543/Python-stack-solution-with-comment-beats-95-on-time-and-space
+
+
+class Solution:
+    def str2tree(self, s: str) -> TreeNode:
+        stack = []
+        num = ''
+        root = None
+
+        for idx, char in enumerate(s):
+            if char == '(':
+                pass
+            elif char == ')':
+                stack.pop()
             else:
-                node = TreeNode(int(cur))
-                if stack:
-                    if not stack[-1].left:
-                        stack[-1].left = node
+                num += char
+                if idx + 1 >= len(s) or not s[idx + 1].isdigit():
+                    curr = TreeNode(int(num))
+                    num = ''
+                    if not stack:
+                        root = curr
                     else:
-                        stack[-1].right = node
-                cur = ""
-                if c == "(":
-                    stack.append(node)
-        return stack and stack[0] or (cur and TreeNode(int(cur))) or []
+                        parent = stack[-1]
+                        if not parent.left:
+                            parent.left = curr
+                        else:
+                            parent.right = curr
 
+                    stack.append(curr)
 
-
-
-class Solution3:
-    def str2tree(self, s):
-        """
-        :type s: str
-        :rtype: TreeNode
-        """
-        if not s: return None
-        n = ''
-        while s and s[0] not in ('(', ')'):
-            n += s[0]
-            s = s[1:]
-        node = TreeNode(int(n))
-        left, right = self.divide(s)
-        node.left = self.str2tree(left[1:-1])
-        node.right = self.str2tree(right[1:-1])
-        return node
-
-    def divide(self, s):
-        part, deg = '', 0
-        while s:
-            deg += {'(' : 1, ')' : -1}.get(s[0], 0)
-            part += s[0]
-            s = s[1:]
-            if deg == 0: break
-        return part, s
+        return root
 
 
