3da06858fe31ed56339da12befc0fc6df059dc68
taocheng <taocheng984@gmail.com>
2019-11-02 22:16:03 -0400

    LC_257_Binary_Tree_Paths.py

--- LeetcodeNew/Tree/LC_257_Binary_Tree_Paths.py
+++ LeetcodeNew/Tree/LC_257_Binary_Tree_Paths.py
@@ -18,107 +18,27 @@ Explanation: All root-to-leaf paths are: 1->2->5, 1->3
 
 import collections
 
-class SolutionCaikehe:
-    # dfs + stack
-    def binaryTreePaths1(self, root):
-        if not root:
-            return []
-        res, stack = [], [(root, "")]
-        while stack:
-            node, ls = stack.pop()
-            if not node.left and not node.right:
-                res.append(ls + str(node.val))
-            if node.right:
-                stack.append((node.right, ls + str(node.val) + "->"))
-            if node.left:
-                stack.append((node.left, ls + str(node.val) + "->"))
-        return res
-
-
-    # bfs + queue
-    def binaryTreePaths2(self, root):
-        if not root:
-            return []
-        res, queue = [], collections.deque([(root, "")])
-        while queue:
-            node, ls = queue.popleft()
-            if not node.left and not node.right:
-                res.append(ls + str(node.val))
-            if node.left:
-                queue.append((node.left, ls + str(node.val) + "->"))
-            if node.right:
-                queue.append((node.right, ls + str(node.val) + "->"))
-        return res
-
-
-    # dfs recursively
-    def binaryTreePaths(self, root):
-        if not root:
-            return []
-        res = []
-        self.dfs(root, "", res)
-        return res
-
-    #Root to node, we only need to maintain one side
-    def dfs(self, root, ls, res):
-        if not root.left and not root.right:
-            res.append(ls + str(root.val))
-        if root.left:
-            self.dfs(root.left, ls + str(root.val) + "->", res)
-        if root.right:
-            self.dfs(root.right, ls + str(root.val) + "->", res)
-
 
 class TreeNode:
-    """ Definition of a binary tree node."""
     def __init__(self, x):
         self.val = x
         self.left = None
         self.right = None
 
-
 class Solution:
-    def binaryTreePaths(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[str]
-        """
-        def construct_paths(root, path):
-            if root:
-                path += str(root.val)
-                if not root.left and not root.right:  # if reach a leaf
-                    paths.append(path)  # update paths
-                else:
-                    path += '->'  # extend the current path
-                    construct_paths(root.left, path)
-                    construct_paths(root.right, path)
+    def binaryTreePaths(self, root: TreeNode) -> List[str]:
+        return self.dfs(root, [], '')
 
-        paths = []
-        construct_paths(root, '')
-        return paths
+    def dfs(self, root, res, path):
 
-class Solution:
-    def binaryTreePaths(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[str]
-        """
         if not root:
-            return []
+            return
+        if root and (not root.left and not root.right):
+            res.append(path + str(root.val))
 
-        paths = []
-        stack = [(root, str(root.val))]
-        while stack:
-            node, path = stack.pop()
-            if not node.left and not node.right:
-                paths.append(path)
-            if node.left:
-                stack.append((node.left, path + '->' + str(node.left.val)))
-            if node.right:
-                stack.append((node.right, path + '->' + str(node.right.val)))
-
-        return paths
-
+        self.dfs(root.left, res, path + str(root.val) + '->')
+        self.dfs(root.right, res, path + str(root.val) + '->')
+        return res
 
 
 
