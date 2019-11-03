e542094b43dfc61e6fb2b598d0eb2d1cdb9eaf42
taocheng <taocheng984@gmail.com>
2019-11-02 21:12:59 -0400

    LC_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py

--- LeetcodeNew/Tree/LC_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
+++ LeetcodeNew/Tree/LC_106_Construct_Binary_Tree_from_Inorder_and_Postorder_Traversal.py
@@ -38,49 +38,16 @@ class TreeNode:
 
 
 class Solution:
-    def buildTree(self, inorder, postorder):
+    def buildTree(self, inorder, postorder) -> TreeNode:
+
         if inorder:
-            ind = inorder.index(postorder.pop())
-            root = TreeNode(inorder[ind])
-            root.right = self.buildTree(inorder[ind + 1:], postorder)
-            root.left = self.buildTree(inorder[:ind], postorder)
+            index = inorder.index(postorder.pop())
+            root = TreeNode(inorder[index])
+            root.right = self.buildTree(inorder[index + 1:], postorder)
+            root.left = self.buildTree(inorder[:index], postorder)
+
             return root
 
 
-
-class Solution2:
-    def buildTree(self, inorder, postorder):
-        map_inorder = {}
-        for i, val in enumerate(inorder): map_inorder[val] = i
-        def recur(low, high):
-            if low > high: return None
-            x = TreeNode(postorder.pop())
-            mid = map_inorder[x.val]
-            x.right = recur(mid+1, high)
-            x.left = recur(low, mid-1)
-            return x
-        return recur(0, len(inorder)-1)
-
-"""
-Same ideas as in Construct Binary Tree from Preorder and Inorder Traversal: 
-
-use inddict, a dictionary of val:index in inorder, to reduce the runtime. 
-
-Geting an item in a dictionary is O(1), but list.index() is O(n) 
-use index pointers Lin, Rin for inorder instead of slicing
-"""
-
-class Solution3:
-    def buildTree(self, inorder, postorder):
-        def helper(Lin, Rin):
-            if Lin < Rin:
-                root = TreeNode(postorder.pop(-1))
-                rootind = inddict[root.val]
-                root.right = helper(rootind + 1, Rin)
-                root.left = helper(Lin, rootind)
-                return root
-
-        inddict = {val: i for i, val in enumerate(inorder)}
-        return helper(0, len(inorder))
 
 
