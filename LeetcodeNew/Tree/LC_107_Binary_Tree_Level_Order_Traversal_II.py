b50f4d6d207a404bc0210fe1c5b27489eae0351b
taocheng <taocheng984@gmail.com>
2019-11-02 21:15:07 -0400

    LC_107_Binary_Tree_Level_Order_Traversal_II.py

--- LeetcodeNew/Tree/LC_107_Binary_Tree_Level_Order_Traversal_II.py
+++ LeetcodeNew/Tree/LC_107_Binary_Tree_Level_Order_Traversal_II.py
@@ -28,77 +28,50 @@ class TreeNode(object):
         self.right = None
 
 
+
+
+
 class Solution:
-    # dfs recursively
-    def levelOrderBottom(self, root):
-        res = []
-        self.dfs(root, 0, res)
+    def levelOrderBottom(self, root: TreeNode):
+
+        if not root:
+            return []
+
+        res = collections.deque()
+        queue = collections.deque([root])
+
+        while queue:
+            size, cur_level = len(queue), []
+
+            for i in range(size):
+                node = queue.popleft()
+                cur_level.append(node.val)
+
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+
+            res.appendleft(cur_level)
+
         return res
 
-    def dfs(self, root, level, res):
-        if root:
-            if len(res) < level + 1:
-                res.insert(0, [])
-            res[-(level + 1)].append(root.val)
-            self.dfs(root.left, level + 1, res)
-            self.dfs(root.right, level + 1, res)
-
-    # dfs + stack
-    def levelOrderBottom1(self, root):
-        stack = [(root, 0)]
-        res = []
-        while stack:
-            node, level = stack.pop()
-            if node:
-                if len(res) < level + 1:
-                    res.insert(0, [])
-                res[-(level + 1)].append(node.val)
-                stack.append((node.right, level + 1))
-                stack.append((node.left, level + 1))
-        return  res
-
-    # bfs + queue
-    def levelOrderBottom2(self, root):
-        queue, res = collections.deque([(root, 0)]), []
-        while queue:
-            node, level = queue.popleft()
-            if node:
-                if len(res) < level + 1:
-                    res.insert(0, [])
-                res[-(level + 1)].append(node.val)
-                queue.append((node.left, level + 1))
-                queue.append((node.right, level + 1))
+    def levelOrderBottom2(self, root: TreeNode):
+        res = collections.deque()
+        if not root:
+            return res
+        self.dfs(root, res, 0)
         return res
 
+    def dfs(self, root, res, level):
+        if not root:
+            return None
 
+        if len(res) <= level:
+            res.appendleft([])
 
-    def levelOrderBottomLee(self, root):
-        queue, res = collections.deque([(root, 0)]), []
-        while queue:
-            node, level = queue.popleft()
-            if node:
-                if level == len(res):
-                    res.append([])
-                res[level].append(node.val)
-                queue.append((node.left, level + 1))
-                queue.append((node.right, level + 1))
-        return res[::-1]
-
-    def levelOrderBottomLee2(self, root):
-        res, queue = [], [root]
-        while queue:
-            res.append([node.val for node in queue if node])
-            queue = [child for node in queue if node for child in (node.left, node.right)]
-        return res[-2::-1]
-
-
-if __name__ == "__main__":
-    root = TreeNode(3)
-    root.left = TreeNode(9)
-    root.right = TreeNode(20)
-    root.right.left = TreeNode(15)
-    root.right.right = TreeNode(7)
-    result = Solution().levelOrderBottom(root)
-    print(result)
+        res[len(res) - 1 - level].append(root.val)
+        self.dfs(root.left, res, level + 1)
+        self.dfs(root.right, res, level + 1)
 
 
