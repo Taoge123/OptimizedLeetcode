76eef636f127778781d1fef29401172961e3921e
taocheng <taocheng984@gmail.com>
2019-11-02 22:26:41 -0400

    LC_449_Serialize_and_Deserialize_BST.py

--- LeetcodeNew/Tree/LC_449_Serialize_and_Deserialize_BST.py
+++ LeetcodeNew/Tree/LC_449_Serialize_and_Deserialize_BST.py
@@ -11,127 +11,36 @@ class TreeNode(object):
 class Codec:
 
     def serialize(self, root):
-        vals = []
 
-        def preOrder(node):
-            if node:
-                vals.append(node.val)
-                preOrder(node.left)
-                preOrder(node.right)
+        preorder = []
+        self.helper(root, preorder)
+        print(preorder)
+        return ' '.join(map(str, preorder))
 
-        preOrder(root)
-
-        return ' '.join(map(str, vals))
-
-    # O( N ) since each val run build once
-    def deserialize(self, data):
-        vals = collections.deque(int(val) for val in data.split())
-
-        def build(minVal, maxVal):
-            if vals and minVal < vals[0] < maxVal:
-                val = vals.popleft()
-                node = TreeNode(val)
-                node.left = build(minVal, val)
-                node.right = build(val, maxVal)
-                return node
-
-        return build(float('-infinity'), float('infinity'))
-
-
-class Codec2:
-
-    def serialize(self, root):
-        res, stk = [], []
-        while stk or root:
-            if root:
-                res.append(str(root.val))
-                stk.append(root)
-                root = root.left
-            else:
-                root = stk.pop()
-                root = root.right
-        return ' '.join(res)
+    def helper(self, root, preorder):
+        if not root:
+            return
+        preorder.append(root.val)
+        self.helper(root.left, preorder)
+        self.helper(root.right, preorder)
 
     def deserialize(self, data):
+
         if not data:
             return None
-        data = map(int, data.split(' '))
-        stk = []
-        root = node = TreeNode(data[0])
-        for n in data[1:]:
-            if n < node.val:
-                node.left = TreeNode(n)
-                stk.append(node)
-                node = node.left
-            else:
-                while stk and stk[-1].val < n:
-                    node = stk.pop()
-                node.right = TreeNode(n)
-                node = node.right
-        return root
+        data = collections.deque(int(val) for val in data.split())
+        return self.build(data, float('-inf'), float('inf'))
 
-
-from collections import deque
-
-
-class Codec3:
-    # recursive function to serialize BST
-    # using a pre-order traversal
-    # simply stores node values to a list
-    def serialize_bst(self, root, preorder):
-        if root is None:
-            return
-        preorder.append(str(root.val))
-        self.serialize_bst(root.left, preorder)
-        self.serialize_bst(root.right, preorder)
-
-    # serialize BST to pre-order traversal
-    def serialize(self, root):
-        """Encodes a tree to a single string.
-
-        :type root: TreeNode
-        :rtype: str
-        """
-        preorder = []
-        self.serialize_bst(root, preorder)
-        return ','.join(preorder)
-
-    # recursive function to deserialize BST from preorder queue
-    def deserialize_bst(self, queue, maximum):
-        # base case: empty queue
-        if len(queue) == 0:
+    def build(self, preorder, min, max):
+        if not preorder:
             return None
 
-        # if the head of the queue is too big
-        # then this node is not meant to be placed here
-        if int(queue[0]) > maximum:
-            return None
-
-        # create root node
-        root = TreeNode(int(queue.popleft()))
-
-        # create left and right subtrees
-        root.left = self.deserialize_bst(queue, root.val)
-        root.right = self.deserialize_bst(queue, maximum)
-
-        # return root node
-        return root
-
-    # deserialize BST pre-order traversal
-    def deserialize(self, data):
-        """Decodes your encoded data to tree.
-
-        :type data: str
-        :rtype: TreeNode
-        """
-        # sanity check: empty input be ignored, don't creat queue
-        if len(data) == 0:
-            return None
-        queue = deque(data.split(','))
-        # the maximum value is infinity
-        root = self.deserialize_bst(queue, float('inf'))
-        return root
-
+        if preorder and preorder[0] > min and preorder[0] < max:
+            val = preorder.popleft()
+            root = TreeNode(val)
+            root.left = self.build(preorder, min, root.val)
+            root.right = self.build(preorder, root.val, max)
+            return root
 
 
 
