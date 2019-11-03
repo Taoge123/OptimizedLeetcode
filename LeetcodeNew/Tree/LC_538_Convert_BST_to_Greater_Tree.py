a0e1e96466ff27ae2b397022dacc33e7dc48d363
taocheng <taocheng984@gmail.com>
2019-11-02 22:38:06 -0400

    LC_538_Convert_BST_to_Greater_Tree.py

--- LeetcodeNew/Tree/LC_538_Convert_BST_to_Greater_Tree.py
+++ LeetcodeNew/Tree/LC_538_Convert_BST_to_Greater_Tree.py
@@ -18,43 +18,6 @@ Output: The root of a Greater Tree like this:
 
 """
 
-class Solution:
-    def __init__(self):
-        self.total = 0
-
-    def convertBST(self, root):
-        if root is not None:
-            self.convertBST(root.right)
-            self.total += root.val
-            root.val = self.total
-            self.convertBST(root.left)
-        return root
-
-
-class Solution2:
-    def convertBST(self, root):
-        total = 0
-
-        node = root
-        stack = []
-        while stack or node is not None:
-            # push all nodes up to (and including) this subtree's maximum on
-            # the stack.
-            while node is not None:
-                stack.append(node)
-                node = node.right
-
-            node = stack.pop()
-            total += node.val
-            node.val = total
-
-            # all nodes with values between the current and its parent lie in
-            # the left subtree.
-            node = node.left
-
-        return root
-
-
 """
 ntuition
 
@@ -96,44 +59,27 @@ so we can now erase the temporary link and move into the left subtree.
 
 """
 
-class Solution3:
-    def convertBST(self, root):
-        # Get the node with the smallest value greater than this one.
-        def get_successor(node):
-            succ = node.right
-            while succ.left is not None and succ.left is not node:
-                succ = succ.left
-            return succ
 
-        total = 0
-        node = root
-        while node is not None:
-            # If there is no right subtree, then we can visit this node and
-            # continue traversing left.
-            if node.right is None:
-                total += node.val
-                node.val = total
-                node = node.left
-            # If there is a right subtree, then there is a node that has a
-            # greater value than the current one. therefore, we must traverse
-            # that node first.
-            else:
-                succ = get_successor(node)
-                # If there is no left subtree (or right subtree, because we are
-                # in this branch of control flow), make a temporary connection
-                # back to the current node.
-                if succ.left is None:
-                    succ.left = node
-                    node = node.right
-                # If there is a left subtree, it is a link that we created on
-                # a previous pass, so we should unlink it and visit this node.
-                else:
-                    succ.left = None
-                    total += node.val
-                    node.val = total
-                    node = node.left
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
+class Solution:
+    def convertBST(self, root: TreeNode) -> TreeNode:
+        self.sum = 0
+        self.helper(root)
         return root
+
+    def helper(self, root):
+        if not root:
+            return
+
+        self.helper(root.right)
+        root.val += self.sum
+        self.sum = root.val
+        self.helper(root.left)
 
 
 
