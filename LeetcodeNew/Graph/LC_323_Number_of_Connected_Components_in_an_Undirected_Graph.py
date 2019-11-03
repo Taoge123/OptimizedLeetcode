1c8e372151ebd7d19b877998cc7cbdd849bcd4cd
taocheng <taocheng984@gmail.com>
2019-11-03 13:05:57 -0500

    LC_323_Number_of_Connected_Components_in_an_Undirected_Graph.py

--- LeetcodeNew/Graph/LC_323_Number_of_Connected_Components_in_an_Undirected_Graph.py
+++ LeetcodeNew/Graph/LC_323_Number_of_Connected_Components_in_an_Undirected_Graph.py
@@ -27,77 +27,81 @@ Since all edges are undirected, [0, 1] is the same as [1, 0]
 and thus will not appear together in edges.
 """
 
-#DFS
 import collections
-class SolutionDFS:
-    def countComponents(n, edges):
-        def dfs(n, graph, visited):
-            if visited[n]:
-                return
-            visited[n] = 1
-            for x in graph[n]:
-                dfs(x, graph, visited)
 
-        visited = [0] * n
-        graph = {x: [] for x in range(n)}
-        #graph = collections.defaultdict(list)
-        for x, y in edges:
-            graph[x].append(y)
-            graph[y].append(x)
 
-        ret = 0
+class Solution:
+    def countComponents(self, n: int, edges) -> int:
+        result = n
+        nums = [-1] * n
+        for u, v in edges:
+            if self.union(nums, u, v):
+                result -= 1
+        return result
+
+    def find(self, nums, i):
+        if nums[i] == -1:
+            return i
+        return self.find(nums, nums[i])
+
+    def union(self, nums, i, j):
+        x, y = self.find(nums, i), self.find(nums, j)
+        if x == y:
+            return False
+        nums[x] = y
+        return True
+
+
+class Solution:
+    def countComponents(self, n: int, edges) -> int:
+        result = n
+        nums = [i for i in range(n)]
+
+        for edge in edges:
+            if self.unite(edge[0], edge[1], nums):
+                result -= 1
+
+        return result
+
+
+    def find(self, nums, i):
+        while nums[i] != i: # 假如不相同，意味着start已经被归入了集合
+            nums[i] = nums[nums[i]] # 往下深入，寻找最终的id
+            i = nums[i] #
+        return i
+
+    def unite(self, i, j, nums):
+        x, y = self.find(nums, i), self.find(nums, j)
+        if x == y: # 同一个集合
+            return False
+        else: # 不同id，连接成一个集合
+            nums[x] = y
+            return True
+
+
+class Solution:
+    def countComponents(self, n: int, edges) -> int:
+        graph = collections.defaultdict(list)
+        visited = set()
+        res = 0
+        for u, v in edges:
+            graph[u].append(v)
+            graph[v].append(u)
+
         for i in range(n):
-            if not visited[i]:
-                dfs(i, graph, visited)
-                ret += 1
+            if self.dfs(graph, i, visited):
+                res += 1
+        return res
 
-        return ret
+    def dfs(self, graph, node, visited):
 
+        if node in visited:
+            return False
+        visited.add(node)
+        for i in graph[node]:
+            self.dfs(graph, i, visited)
 
-class SolutionBFS:
-    def countComponents(n, edges):
-        g = {x: [] for x in range(n)}
-        for x, y in edges:
-            g[x].append(y)
-            g[y].append(x)
-
-        ret = 0
-        for i in range(n):
-            queue = [i]
-            ret += 1 if i in g else 0
-            for j in queue:
-                if j in g:
-                    queue += g[j]
-                    del g[j]
-
-        return ret
-
-
-class SolutionUF:
-    def countComponents(n, edges):
-        def find(x):
-            if parent[x] != x:
-                parent[x] = find(parent[x])
-            return parent[x]
-
-        def union(xy):
-            x, y = map(find, xy)
-            if rank[x] < rank[y]:
-                parent[x] = y
-            else:
-                parent[y] = x
-                if rank[x] == rank[y]:
-                    rank[x] += 1
-
-        parent, rank = range(n), [0] * n
-        map(union, edges)
-        return len({find(x) for x in parent})
-
-
-
-
-
-
+        return True
 
 
 
