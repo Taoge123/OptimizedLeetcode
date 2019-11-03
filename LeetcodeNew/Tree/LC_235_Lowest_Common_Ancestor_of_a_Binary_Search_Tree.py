4f383267f273fff75d76c3c182d00f22486cf877
taocheng <taocheng984@gmail.com>
2019-11-02 22:13:42 -0400

    LC_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py

--- LeetcodeNew/Tree/LC_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
+++ LeetcodeNew/Tree/LC_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree.py
@@ -46,83 +46,24 @@ https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/sol
 """
 
 
-class SolutionYu:
-    def lowestCommonAncestor(self, root, p, q):
-        if not root: return None
-        if max(p.val, q.val) < root.val:
-            return self.lowestCommonAncestor(root.left, p, q)
-        if min(p.val, q.val) > root.val:
-            return self.lowestCommonAncestor(root.right, p, q)
-        return root
-
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution:
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        # Value of current node or parent node.
-        parent_val = root.val
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root:
+            return
 
-        # Value of p
-        p_val = p.val
-
-        # Value of q
-        q_val = q.val
-
-        # If both p and q are greater than parent
-        if p_val > parent_val and q_val > parent_val:
-            return self.lowestCommonAncestor(root.right, p, q)
-        # If both p and q are lesser than parent
-        elif p_val < parent_val and q_val < parent_val:
+        if root.val > max(p.val, q.val):
             return self.lowestCommonAncestor(root.left, p, q)
-        # We have found the split point, i.e. the LCA node.
-        else:
-            return root
 
+        if root.val < min(p.val, q.val):
+            return self.lowestCommonAncestor(root.right, p, q)
 
-
-class Solution2:
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-
-        # Value of p
-        p_val = p.val
-
-        # Value of q
-        q_val = q.val
-
-        # Start from the root node of the tree
-        node = root
-
-        # Traverse the tree
-        while node:
-
-            # Value of current node or parent node.
-            parent_val = node.val
-
-            if p_val > parent_val and q_val > parent_val:
-                # If both p and q are greater than parent
-                node = node.right
-            elif p_val < parent_val and q_val < parent_val:
-                # If both p and q are lesser than parent
-                node = node.left
-            else:
-                # We have found the split point, i.e. the LCA node.
-                return node
-
-
-
-
-
+        return root
 
 
 
