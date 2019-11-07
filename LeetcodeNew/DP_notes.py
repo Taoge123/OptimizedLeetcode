6f44cab8fde04cb192a0e5a096853ed95f26b677
taocheng <taocheng984@gmail.com>
2019-11-06 23:18:09 -0500

    ../DP_notes.py

--- LeetcodeNew/DP_notes.py
+++ LeetcodeNew/DP_notes.py
@@ -292,7 +292,7 @@ else:
 
 
 -----------------------------------------------------------------------------------------------
-problem 10:
+problem 11:
 Coin Changing Number of ways to get total
 
 coins 1 2 3   total = 5
@@ -311,7 +311,7 @@ else:
 
 
 -----------------------------------------------------------------------------------------------
-problem 10:
+problem 12:
 Egg Dropping Dynamic Programming
 
 
@@ -359,7 +359,7 @@ if doesn't, i eggs left and j-k floors left
 
 
 -----------------------------------------------------------------------------------------------
-problem 11:
+problem 13:
 Weighted Job Scheduling Dynamic Programming
 
   j                               i
@@ -374,6 +374,281 @@ for i in range(1, len):
         if i.start >= j.end:
             T[i] = max(T[i], T[j] + profit[i])            
 
+-----------------------------------------------------------------------------------------------
+problem 14:
+Regular Expression Dynamic Programming
+
+* match 0 or more occurrence of charactoers before * 
+. match nay single character
+
+a.b 
+acb aab axb -- True
+ab  acby cb -- False
+
+a*b.*y 
+by, bly, ably, ablmy -- True
+ay, ab -- False
+
+if s[i] == pattern[j] or p[j] == '.':
+    T[i][j] = T[i-1][j-1]
+elif p[j] == '*':
+    T[i][j] = T[i][j-2]
+    if p[j-1] == s[i] or p[j-1] == '.':
+        T[i][j] = T[i-1][j]
+else:
+    T[i][j] = False
+
+     0  1  2  3  4  5  6 
+        x  a  *  b  .  c
+0    T  F  F  F  F  F  F   
+1  x F  T  F  T  F  F  F                  
+2  a F  F  T (T) F  F  F             
+3  a F  F  F (T) F  F  F                 
+4  b F  F  F  F  T  F  F                    
+5  y F  F  F  F  F  T  F        
+   c F  F  F  F  F  F  T         
+            
+x a * 
+a          
+                                         
+
+
+-----------------------------------------------------------------------------------------------
+problem 15:
+Maximum Sum Rectangular Submatrix in Matrix dynamic programming/2D kadane
+
+
+
+
+
+
+
+-----------------------------------------------------------------------------------------------
+problem 16:
+Longest Common Substring
+
+a b c d a f 
+  -----
+z b c d f
+-------
+
+      a  b  c  d  a  f 
+   0  0  0  0  0  0  0             
+z  0  0  0  0  0  0  0                     
+b  0  0  1  0  0  0  0                  
+c  0  0  0  2  0  0  0                     
+d  0  0  0  0 (3) 0  0                    
+f  0  0  0  0  0  0  1                    
+
+if s1[i] == s2[j]:
+    T[i][j] = T[i-1][j-1] + 1
+else:
+    T[i][j] = 0
+
+
+
+-----------------------------------------------------------------------------------------------
+problem 17:
+Maximum Size Rectangle of All 1's Dynamic Programming
+
+   0  1  2  3  4  5 
+0  1  0  0  1  1  1                 
+1  1  0  1  1  0  1                  
+2  0  1  1  1  1  1                  
+3  0  0  1  1  1  1                  
+             
+             
+    0  1  2  3  4  5 
+    -----------------
+0   1  0  0  1  1  1
+1   2  0  1  2  0  2                
+2   0  1  2  3  1  3                  
+3   0  0  3  4  2  4                
+      
+maxArea = 8
+
+
+
+
+-----------------------------------------------------------------------------------------------
+problem 18:
+Word Break Problem Dynamic Programming
+
+0  1  2  3  4  5
+I  a  m  a  c  e
+-  ----  -------
+I a m c                 
+                 
+   0  1  2  3  4  5             
+0  T  T  T  T  F  T       
+1     T  T  T  F  T    
+2        T  F  F  F    
+3           T  F  T    
+4              T  F 
+5                 T                 
+                    
+if word[i:j] in dictionary:
+    T[i][j] = True
+else:
+    T[i][j] = True for k in T[i][k] and T[k+1][j] 
+                      
+
+
+                        
+
+-----------------------------------------------------------------------------------------------
+problem 19:
+Buy/Sell Stock With K transactions To Maximize Profit Dynamic Programming
+
+#case1 is not transacting on jth day
+#case2 is completing transaction on jth day
+T[i][j] = max(T[i][j-1],
+              T[i-1][m] + price[j] - price[m] for m in range(0, j) )
+              
+              
+T[i][j] = max(T[i][j-1],
+              prices[j] + maxDiff
+              maxDiff = max(maxDiff, T[i-1][j] - price[j]))
+k = 3
+row is transactions 
+
+j = 3, i = 2
+m = 0, T[1][0] - price[0] + price[3]
+m = 1, T[1][1] - price[1] + price[3]
+m = 2, T[1][2] - price[2] + price[3]
+
+j = 4, i = 2
+m = 0, T[1][0] - price[0] + price[4]
+m = 1, T[1][1] - price[1] + price[4]
+m = 2, T[1][2] - price[2] + price[4]
+m = 3, T[1][3] - price[3] + price[4]
+
+   0  1  2  3  4  5  6  7 
+   2  5  7  1  4  3  1  3
+0  0  0  0  0  0  0  0  0                         
+1  0  3  5  5  5  5  5  5                    
+2  0  3  5  5  8  8  8  8                      
+3  0  3  5  5  8  8  8  10                     
+                           
+                             
+                        
+-----------------------------------------------------------------------------------------------
+problem 20:
+Minimum jump to reach end
+   
+   j                          i                      
+   0  1  2  3  4  5  6  7  8  9
+   2  3  1  1  2  4  2  0  1  1
+                                 
+   0  1  1  2  2  3  3  4  4  4                                         
+                                
+  -1  0  0  1  1  4  4  5  5  5
+
+if i <= nums[j]:
+    T[i] = min(T[i], T[j] + 1)
+
+
+
+-----------------------------------------------------------------------------------------------
+problem 21:
+Minimum Cost Path Dynamic Programming 
+
+--------
+1 3 5 8| 
+4 2 1 7|
+4 3 2 3|
+
+
+1 4 9 17 
+5 6 7 14       
+9 9 9 12
+
+T[i][j] = val[i][j] + min(T[i-1][j], T[i][j-1])
+
+
+
+
+
+
+-----------------------------------------------------------------------------------------------
+problem 22:
+Text Justification Dynamic Programming
+
+0 Tushar - 6
+1 Roy    - 3
+2 like   - 5
+3 to     - 2 
+4 code   - 4
+
+https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/TextJustification.java
+
+
+    0   1   2   3   4
+0  16   0  inf inf inf                
+1       49  1  inf inf       
+2           25  4  inf      
+3               64  9 
+4                   36               
+                 
+         i  j
+         9  36        
+0  1  2  3  4                                       
+         5  5      
+0  1  2  3  4
+              
+               
+C[][]
+
+M[i] = min(M[j] + C[i][j-1]) for j in range(i+1, len)          
+             
+               
+
+-----------------------------------------------------------------------------------------------
+problem 23:
+
+
+if s[i] == p[j] or p[j] == '?':
+    T[i][j] = T[i-1][j-1]
+elif p[j] == '*':
+    T[i][j] = T[i-1][j] or T[i][j-1]
+else:
+    T[i][j] = False
+
+
+     pattern 
+     0   1   2   3   4   5 
+         x   ?   y   *   z
+0    T   F   F   F   F   F                    
+1 x  F   T   F   F   F   F                  
+2 a  F   F   T   F   F   F                  
+3 y  F   F   F   T   T   F                  
+4 l  F   F   F   F   T   F                    
+5 m  F   F   F   F   T   F                    
+6 z  F   F   F   F   T   T                  
+
+
+
+
+-----------------------------------------------------------------------------------------------
+problem 24:
+Palindrome Partition Dynamic Programming
+
+   0   1   2   3   4
+   a   b   c   b   m
+   
+   
+   0  1  2  3  4    
+0  0  1  2  1  2         
+1     0  1  0  1     
+2        0  1  2     
+3           0  1   
+4              0     
+   
+if isPalindrome(i, j):
+    T[i][j] = 0                
+else:
+    T[i][j] = 1 + min(T[i][k] + T[k+1][j]) for k in range(i, j-1)            
+                      
 
 
 
@@ -382,7 +657,7 @@ for i in range(1, len):
 
 
 
-                   
+                
 """
 
 
