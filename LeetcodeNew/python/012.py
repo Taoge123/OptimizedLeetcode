e33053bf49211194a33756cfc10e8e9638fddbc1
taocheng <taocheng984@gmail.com>
2019-11-12 23:17:14 -0500

    012.py

---
+++ LeetcodeNew/python/012.py
@@ -0,0 +1,13 @@
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        numerals = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"]
+        res = ""
+        for i, val in enumerate(values):
+            res += (num // val) * numerals[i]
+            num %= val
+        return res
+
+
+
+
