279b4235deff14692fcd0de735feaf384ba78c6c
taocheng <taocheng984@gmail.com>
2019-11-14 22:53:04 -0500

    LC_013.py

--- LeetcodeNew/python/LC_007.py
+++ LeetcodeNew/python/LC_007.py
@@ -1,10 +1,24 @@
+"""
+Given a 32-bit signed integer, reverse digits of an integer.
 
-class Solution():
+Example 1:
+
+Input: 123
+Output: 321
+Example 2:
+
+Input: -123
+Output: -321
+Example 3:
+
+Input: 120
+Output: 21
+
+"""
+
+class Solution:
     def reverse(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
+
         if x < 0:
             return -self.reverse(-x)
 
