8ef50c179595d4e3d997878ec6fe452275f2d9fb
taocheng <taocheng984@gmail.com>
2019-11-14 23:00:51 -0500

    LC_077.py

--- LeetcodeNew/python/LC_077.py
+++ LeetcodeNew/python/LC_077.py
@@ -1,3 +1,20 @@
+"""
+Given two integers n and k, return all possible combinations of k numbers out of 1 ... n.
+
+Example:
+
+Input: n = 4, k = 2
+Output:
+[
+  [2,4],
+  [3,4],
+  [2,3],
+  [1,2],
+  [1,3],
+  [1,4],
+]
+"""
+
 
 # class Solution:
 #     def combine(self, n, k):
