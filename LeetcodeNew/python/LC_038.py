b238db55679e0a5644e850c8b965073d86166bd3
taocheng <taocheng984@gmail.com>
2019-12-10 21:00:51 -0500

    all

--- LeetcodeNew/python/LC_038.py
+++ LeetcodeNew/python/LC_038.py
@@ -31,7 +31,7 @@ class Solution:
     def countAndSay(self, n: int) -> str:
 
         res = "1"
-        for i in range( n -1):
+        for i in range(n-1):
             curr = res[0]
             temp = ''
             count = 0
