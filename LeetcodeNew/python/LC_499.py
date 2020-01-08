2bed1a362bc7ace02b0d850088238ac88c234754
taocheng <taocheng984@gmail.com>
2020-01-07 22:04:13 -0500

    all

--- LeetcodeNew/python/LC_499.py
+++ LeetcodeNew/python/LC_499.py
@@ -59,7 +59,6 @@ import collections, heapq
 
 class Solution:
     def findShortestWay(self, maze, ball, hole):
-
         m, n = len(maze), len(maze[0])
         queue = [(0, "", ball[0], ball[1])]
         directions = [(-1, 0, 'u'), (1, 0, 'd'), (0, -1, 'l'), (0, 1, 'r')]
@@ -73,7 +72,8 @@ class Solution:
 
             for dir in directions:
                 step, x, y = dist, i, j
-                while 0 <= x + dir[0] < m and 0 <= y + dir[1] < n and maze[x + dir[0]][y + dir[1]] != 1:
+                while 0 <= x + dir[0] < m and 0 <= y + dir[1] < n \
+                        and maze[x + dir[0]][y + dir[1]] != 1:
                     x += dir[0]
                     y += dir[1]
                     step += 1
