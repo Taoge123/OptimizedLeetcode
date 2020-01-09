33c09930be57e30f92288066189ffb7854473806
taocheng <taocheng984@gmail.com>
2020-01-09 08:41:34 -0500

    all

--- LeetcodeNew/python/LC_514.py
+++ LeetcodeNew/python/LC_514.py
@@ -42,6 +42,24 @@ It's guaranteed that string key could always be spelled by rotating the string r
 import collections
 
 
+class SolutionTLE:
+    def findRotateSteps(self, ring: str, key: str) -> int:
+        n, m = len(ring), len(key)
+        dp = [[0 for i in range(n)] for j in range(m + 1)]
+
+        for i in range(m - 1, -1, -1):
+            for j in range(n):
+                dp[i][j] = float('inf')
+                for k in range(n):
+                    if ring[k] == key[i]:
+                        diff = abs(j - k)
+                        step = min(diff, n - diff)
+                        dp[i][j] = min(dp[i][j], step + dp[i + 1][k])
+
+        return dp[0][0] + m
+
+
+
 class Solution1:
     def findRotateSteps(self, ring: str, key: str) -> int:
         table = collections.defaultdict(list)
