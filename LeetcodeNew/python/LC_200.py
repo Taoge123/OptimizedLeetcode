8f634ce094fbe6b6eecdd1a71974f2c0c2f5f928
taocheng <taocheng984@gmail.com>
2020-01-12 10:31:51 -0500

    all

--- LeetcodeNew/python/LC_200.py
+++ LeetcodeNew/python/LC_200.py
@@ -73,6 +73,61 @@ class Solution2:
         return res
 
 
+class UnionFind:
+    def __init__(self, grid):
+        m, n = len(grid), len(grid[0])
+        self.count = 0
+        self.parent = [-1] * (m * n)
+        self.rank = [0] * (m * n)
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    self.parent[i * n + j] = i * n + j
+                    self.count += 1
+
+    def find(self, i):
+        if self.parent[i] != i:
+            self.parent[i] = self.find(self.parent[i])
+        return self.parent[i]
+
+    def union(self, x, y):
+        rootx = self.find(x)
+        rooty = self.find(y)
+        if rootx != rooty:
+            if self.rank[rootx] > self.rank[rooty]:
+                self.parent[rooty] = rootx
+            elif self.rank[rootx] < self.rank[rooty]:
+                self.parent[rootx] = rooty
+            else:
+                self.parent[rooty] = rootx
+                self.rank[rootx] += 1
+            self.count -= 1
+
+
+
+class Solution3:
+    def isValid(self, grid, r, c):
+        m, n = len(grid), len(grid[0])
+        if r < 0 or c < 0 or r >= m or c >= n:
+            return False
+        return True
+
+    def numIslands(self, grid):
+        if not grid or not grid[0]:
+            return 0
+        uf = UnionFind(grid)
+
+        directions = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == '1':
+                    for d in directions:
+                        x, y = i + d[0], j + d[1]
+                        if self.isValid(grid, x, y) and grid[x][y] == '1':
+                            uf.union(i * n + j, x * n + y)
+        return uf.count
+
 
 
 
