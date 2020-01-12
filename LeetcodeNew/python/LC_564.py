a3f7a10fda4ae90b3c4bdb0921022e86fa33896f
taocheng <taocheng984@gmail.com>
2020-01-12 14:06:09 -0500

    all

--- LeetcodeNew/python/LC_564.py
+++ LeetcodeNew/python/LC_564.py
@@ -6,7 +6,7 @@ The 'closest' is defined as absolute difference minimized between two integers.
 
 Example 1:
 Input: "123"
-Output: "121"
+res: "121"
 Note:
 The input n is a positive integer represented by string, whose length will not exceed 18.
 If there is a tie, return the smaller one as answer.
@@ -26,10 +26,10 @@ class Solution:
                 return '9'
             else:
                 options = [n[0] * 2, str(int(n[0]) - 1) * 2, str(int(n[0]) + 1) * 2]
-                output = filter(lambda x: x != n, options)
-                diff = [abs(int(x) - int(n)) for x in output]
-                output = filter(lambda x: abs(int(x) - int(n)) == min(diff), options)
-                return output[0]
+                res = filter(lambda x: x != n, options)
+                diff = [abs(int(x) - int(n)) for x in res]
+                res = filter(lambda x: abs(int(x) - int(n)) == min(diff), options)
+                return res[0]
         else:
             m = n[len(n) / 2 - 1:len(n) / 2 + 2] if len(n) % 2 != 0 else n[len(n) / 2 - 2:len(n) / 2 + 2]
             front = n[:(len(n) - len(m)) / 2]
@@ -56,12 +56,12 @@ class Solution:
             p.append('9' * (len(n) - 1))
             p.append('1' + '0' * (len(n) - 1) + '1')
 
-            output = filter(lambda x: x != n, p)
-            diff = [abs(int(x) - int(n)) for x in output]
-            output = filter(lambda x: abs(int(x) - int(n)) == min(diff), output)
-            output = map(lambda x: int(x), output)
-            output.sort()
-            return str(output[0])
+            res = filter(lambda x: x != n, p)
+            diff = [abs(int(x) - int(n)) for x in res]
+            res = filter(lambda x: abs(int(x) - int(n)) == min(diff), res)
+            res = map(lambda x: int(x), res)
+            res.sort()
+            return str(res[0])
 
 
 
