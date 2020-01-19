2aa3fe081d5ea0d8a5cfc3e22c1d76df23f359e0
taocheng <taocheng984@gmail.com>
2020-01-19 13:26:16 -0500

    all

--- LeetcodeNew/python/LC_093.py
+++ LeetcodeNew/python/LC_093.py
@@ -30,6 +30,37 @@ class Solution:
                     self.dfs(s[i:], index + 1, path + s[:i] + ".", res)
 
 
+
+class Solution2:
+    def restoreIpAddresses(self, s: str):
+
+        res = []
+        self.helper(res, s, 0, "", 0)
+        return res
+
+    def helper(self, res, s, index, path, count):
+        if count > 4:
+            return
+
+        if count == 4 and index == len(s):
+            res.append(path)
+            return
+
+        for i in range(1, 4):
+            if index + i > len(s):
+                break
+            temp = s[index:index + i]
+
+            if temp.startswith("0") and len(temp) > 1 or (i == 3 and int(temp) >= 256):
+                continue
+            if count == 3:
+                self.helper(res, s, index + i, path + temp + "", count + 1)
+            else:
+                self.helper(res, s, index + i, path + temp + ".", count + 1)
+
+
+
+
 s = "25525511135"
 a = Solution()
 print(a.restoreIpAddresses(s))
