5e7f3ffff0afe5bda5d7a5d09193eed654179747
taocheng <taocheng984@gmail.com>
2020-01-18 20:50:27 -0500

    all

--- LeetcodeNew/python/LC_063.py
+++ LeetcodeNew/python/LC_063.py
@@ -28,16 +28,12 @@ There are two ways to reach the bottom-right corner:
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid):
         m, n = len(obstacleGrid), len(obstacleGrid[0])
-        dp = [[0 for i in range( n +1)] for j in range( m +1)]
+        dp = [[0 for i in range(n+1)] for j in range(m+1)]
         dp[0][1] = 1
-        for i in range(1, m+ 1):
-            for j in range(1, n + 1):
-                if obstacleGrid[i - 1][j - 1] == 0:
-                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                if obstacleGrid[i-1][j-1] == 0:
+                    dp[i][j] = dp[i][j-1] + dp[i-1][j]
         return dp[m][n]
-
-
-
-
 
 
