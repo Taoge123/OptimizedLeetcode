189f24ff6b990520dabb31160e93439696b69908
taocheng <taocheng984@gmail.com>
2020-01-19 13:14:14 -0500

    all

--- LeetcodeNew/python/LC_091.py
+++ LeetcodeNew/python/LC_091.py
@@ -25,13 +25,13 @@ class Solution:
         if s == "":
             return 0
         n = len(s)
-        dp = [0 for x in range( n +1)]
+        dp = [0 for x in range(n +1)]
         dp[0] = 1
 
         for i in range(1, n + 1):
             if s[i -1] != '0':
                 dp[i] += dp[i-1]
-            if i != 1 and s[i-2:i] >= "10" and s[i -2:i] <= "26":
+            if i != 1 and s[i-2:i] >= "10" and s[i-2:i] <= "26":
                 dp[i] += dp[i-2]
 
         return dp[-1]
