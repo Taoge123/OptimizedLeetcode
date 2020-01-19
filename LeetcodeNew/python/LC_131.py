dd46e13308a0cbd6b3d71b808821af0f9800b2d3
taocheng <taocheng984@gmail.com>
2020-01-19 16:22:03 -0500

    all

--- LeetcodeNew/python/LC_131.py
+++ LeetcodeNew/python/LC_131.py
@@ -36,6 +36,24 @@ class Solution:
         return s == s[::-1]
 
 
+class Solution2:
+    def partition(self, s):
+        res = []
+        self.dfs(s, [], 1, res)
+        return res
+
+    def dfs(self, s, path, index, res):
+        if index == len(s) + 1:
+            res.append(path[:])
+            return
+        for i in range(index, len(s) + 1):
+            if self.isPalindrome(s[index - 1:i]):
+                path.append(s[index - 1:i])
+                self.dfs(s, path, i + 1, res)
+                path.pop()
+
+    def isPalindrome(self, s):
+        return s == s[::-1]
 
 
 
--- LeetcodeNew/python/LC_132.py
+++ LeetcodeNew/python/LC_132.py
@@ -15,9 +15,9 @@ Explanation: The palindrome partitioning ["aa","b"] could be produced using 1 cu
 class Solution:
     def minCut(self, s: str) -> int:
 
-        f = [-1] + [len(s ) -1 for _ in range(len(s))]
+        f = [-1] + [len(s) -1 for _ in range(len(s))]
         for i in range(len(s)):
-            for j in range( i +1 ,len(s ) +1):
+            for j in range(i+1 ,len(s) +1):
                 if s[i:j] == s[i:j][::-1]:
                     f[j] = min(f[j], f[i] + 1)
         return f[-1]
