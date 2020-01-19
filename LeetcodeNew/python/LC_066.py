7d254267120f7328c6e73fc28d632868adad63a0
taocheng <taocheng984@gmail.com>
2020-01-19 11:51:44 -0500

    all

--- LeetcodeNew/python/LC_066.py
+++ LeetcodeNew/python/LC_066.py
@@ -38,5 +38,24 @@ class Solution:
         return digits
 
 
+class Solution2:
+    def plusOne(self, digits):
+
+        carry = 1
+
+        for i in range(len(digits) - 1, -1, -1):
+            if digits[i] + carry > 9:
+                digits[i] = 0
+                carry = 1
+            else:
+                #We can return here, no need to go further
+                digits[i] += carry
+                return digits
+
+        if carry == 1:
+            digits.insert(0, 1)
+
+        return digits
+
 
 
