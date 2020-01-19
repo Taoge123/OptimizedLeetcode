a0f1f21956d5fb301cf8a6279e4c359120faec3c
taocheng <taocheng984@gmail.com>
2020-01-19 12:42:15 -0500

    all

--- LeetcodeNew/python/LC_070.py
+++ LeetcodeNew/python/LC_070.py
@@ -30,7 +30,7 @@ class Solution:
         dp[0] = dp[1] = 1
 
         for i in range(2, n + 1):
-            dp[i] = dp[ i -1] + dp[ i -2]
+            dp[i] = dp[i-1] + dp[i-2]
         return dp[-1]
 
 
