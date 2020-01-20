78f961e2282d21554e6ca307e7c73c625aea6753
taocheng <taocheng984@gmail.com>
2020-01-19 22:39:36 -0500

    all

--- LeetcodeNew/python/LC_174.py
+++ LeetcodeNew/python/LC_174.py
@@ -33,15 +33,15 @@ class Solution:
         m, n = len(dungeon), len(dungeon[0])
         dp = [[0 for i in range(n)] for j in range(m)]
         dp[-1][-1]= max(1, 1 - dungeon[-1][-1])
-        for j in range( n -2, -1, -1):
-            dp[-1][j] = max(dp[-1][j +1] - dungeon[-1][j], 1)
+        for j in range(n-2, -1, -1):
+            dp[-1][j] = max(dp[-1][j+1] - dungeon[-1][j], 1)
 
         for i in range(m-2, -1, -1):
             dp[i][-1] = max(dp[i +1][-1] - dungeon[i][-1], 1)
 
         for i in range(m-2, -1, -1):
             for j in range(n-2, -1, -1):
-                dp[i][j] = max(min(dp[i +1][j], dp[i][j +1]) - dungeon[i][j], 1)
+                dp[i][j] = max(min(dp[i +1][j], dp[i][j+1]) - dungeon[i][j], 1)
         return dp[0][0]
 
 
