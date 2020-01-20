4101db2e4b93bf54117288d72cca8e90d9f9531c
taocheng <taocheng984@gmail.com>
2020-01-20 16:28:46 -0500

    all

--- LeetcodeNew/python/LC_251.py
+++ LeetcodeNew/python/LC_251.py
@@ -48,10 +48,30 @@ class Vector2D:
     def hasNext(self) -> bool:
         return self.size > 0
 
-# Your Vector2D object will be instantiated and called as such:
-# obj = Vector2D(v)
-# param_1 = obj.next()
-# param_2 = obj.hasNext()
+
+class Vector2D2:
+    def __init__(self, vec2d):
+        self.col = 0
+        self.row = 0
+        self.vec = vec2d
+
+    def next(self):
+        if self.hasNext():
+            result = self.vec[self.row][self.col]
+            self.col += 1
+            return result
+
+    def hasNext(self):
+        while self.row < len(self.vec):
+            if self.col < len(self.vec[self.row]):
+                return True
+
+            self.col = 0
+            self.row += 1
+
+        return False
+
+
 
 
 
