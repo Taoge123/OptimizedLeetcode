e984f6eeb74ec4b1138bcb01cd864560c9e2f0d2
taocheng <taocheng984@gmail.com>
2020-01-19 21:41:56 -0500

    all

--- LeetcodeNew/python/LC_155.py
+++ LeetcodeNew/python/LC_155.py
@@ -38,12 +38,6 @@ class MinStack:
         return self.stack[-1][1]
 
 
-# Your MinStack object will be instantiated and called as such:
-# obj = MinStack()
-# obj.push(x)
-# obj.pop()
-# param_3 = obj.top()
-# param_4 = obj.getMin()
 
 
 
