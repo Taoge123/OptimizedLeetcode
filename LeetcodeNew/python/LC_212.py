d3c5830a69a76ddfbcd638fbda010d6d7fb3a0a2
taocheng <taocheng984@gmail.com>
2020-01-20 12:34:19 -0500

    all

--- LeetcodeNew/python/LC_212.py
+++ LeetcodeNew/python/LC_212.py
@@ -80,6 +80,15 @@ class Solution:
 
 
 
+board = [
+  ['o','a','a','n'],
+  ['e','t','a','e'],
+  ['i','h','k','r'],
+  ['i','f','l','v']
+]
+words = ["oath","pea","eat","rain"]
 
+a = Solution()
+print(a.findWords(board, words))
 
 
--- LeetcodeNew/python/LC_213.py
+++ LeetcodeNew/python/LC_213.py
@@ -42,11 +42,29 @@ class Solution:
         res = dp[1]
 
         for i in range(2, n):
-            dp[i] = max(dp[ i -2] + nums[i], dp[ i -1])
+            dp[i] = max(dp[i-2] + nums[i], dp[i -1])
             if dp[i] > res:
                 res = dp[i]
         return res
 
+
+
+class Solution2:
+    def rob(self, nums):
+        n = len(nums)
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+
+        return max(self.helper(nums[1:]), self.helper(nums[:-1]))
+
+
+    def helper(self, nums):
+        rob = noRob = 0
+        for i in range(len(nums)):
+            rob, noRob = max(nums[i] + noRob, rob), rob
+        return rob
 
 
 
