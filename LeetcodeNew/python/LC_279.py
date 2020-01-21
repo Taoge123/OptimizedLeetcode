0e66f1687b71894b58f01d1620ef90f916d506ad
taocheng <taocheng984@gmail.com>
2020-01-20 20:35:43 -0500

    all

--- LeetcodeNew/python/LC_279.py
+++ LeetcodeNew/python/LC_279.py
@@ -20,7 +20,7 @@ class Solution:
         dp = [n ] *( n +1)
         dp[0] = 0
         dp[1] = 1
-        for i in range(2, n+ 1):
+        for i in range(2, n+1):
             j = 1
             while j * j <= i:
                 # print(i, i - j * j)
