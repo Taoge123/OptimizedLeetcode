991b1cfb4d48d07886610f07538d8ecd6908cc1c
taocheng <taocheng984@gmail.com>
2020-01-25 15:46:18 -0500

    all

--- LeetcodeNew/python/LC_229.py
+++ LeetcodeNew/python/LC_229.py
@@ -29,5 +29,42 @@ class Solution:
         return res
 
 
+class Solution2:
+    def majorityElement(self, nums):
+        res = []
+        a, b, count1, count2 = 0, 0, 0, 0
+        n = len(nums)
+
+        for num in nums:
+            if num == a:
+                count1 += 1
+            elif num == b:
+                count2 += 1
+            elif count1 == 0:
+                a = num
+                count1 = 1
+            elif count2 == 0:
+                b = num
+                count2 = 1
+            else:
+                count1 -= 1
+                count2 -= 1
+
+        count1 = count2 = 0
+        for num in nums:
+            if num == a:
+                count1 += 1
+            elif num == b:
+                count2 += 1
+
+        if count1 > n / 3:
+            res.append(a)
+        if count2 > n / 3:
+            res.append(b)
+
+        return res
+
+
+
 
 
