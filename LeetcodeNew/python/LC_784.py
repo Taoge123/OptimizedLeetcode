af081b8ac98793249021f32845dcc81a1e471e34
taocheng <taocheng984@gmail.com>
2020-02-15 15:29:09 -0500

    all

--- LeetcodeNew/python/LC_784.py
+++ LeetcodeNew/python/LC_784.py
@@ -5,15 +5,17 @@ class Solution:
         self.helper(S, "", res)
         return res
 
-    def helper(self, s, p, res):
-        if s == "":
-            res.append(p)
+    def helper(self, s, path, res):
+        if not s:
+            res.append(path)
             return
+
         if s[0].isdigit():
-            self.helper(s[1:], p+ s[0], res)
+            self.helper(s[1:], path + s[0], res)
         else:
-            self.helper(s[1:], p + s[0].upper(), res)
-            self.helper(s[1:], p + s[0].lower(), res)
+            self.helper(s[1:], path + s[0].lower(), res)
+            self.helper(s[1:], path + s[0].upper(), res)
+
 
 
 
