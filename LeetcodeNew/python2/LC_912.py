3e16fc09f24beea0dd1a0111427f48e5462a53d4
taocheng <taocheng984@gmail.com>
2020-02-23 13:58:59 -0500

    all

--- LeetcodeNew/python2/LC_912.py
+++ LeetcodeNew/python2/LC_912.py
@@ -5,21 +5,21 @@ class Solution:
         self.quicksort(nums, 0, len(nums) - 1)
         return nums
 
-    def partition(self, nums, left, high):
-        pivot = nums[high]
+    def partition(self, nums, left, right):
+        pivot = nums[right]
         i = left
-        for j in range(left, high):
+        for j in range(left, right):
             if nums[j] < pivot:
                 nums[i], nums[j] = nums[j], nums[i]
                 i += 1
-        nums[i], nums[high] = nums[high], nums[i]
+        nums[i], nums[right] = nums[right], nums[i]
         return i
 
-    def quicksort(self, nums, left, high):
-        if left < high:
-            pivot = self.partition(nums, left, high)
+    def quicksort(self, nums, left, right):
+        if left < right:
+            pivot = self.partition(nums, left, right)
             self.quicksort(nums, left, pivot - 1)
-            self.quicksort(nums, pivot + 1, high)
+            self.quicksort(nums, pivot + 1, right)
 
 
 
