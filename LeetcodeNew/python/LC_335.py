8d38b5b36032946caad19adba4f6a94ee5a7d325
taocheng <taocheng984@gmail.com>
2020-07-05 20:54:19 -0700

    all

--- LeetcodeNew/python/LC_335.py
+++ LeetcodeNew/python/LC_335.py
@@ -43,8 +43,7 @@ class Solution:
                 return True
             if i >= 4 and x[i - 1] == x[i - 3] and x[i] + x[i - 4] >= x[i - 2]:
                 return True
-            if i >= 5 and x[i - 1] <= x[i - 3] and x[i - 3] <= x[i - 1] + x[i - 5] and x[i] + x[i - 4] >= x[i - 2] and \
-                    x[i - 4] <= x[i - 2]:
+            if i >= 5 and x[i - 1] <= x[i - 3] and x[i - 3] <= x[i - 1] + x[i - 5] and x[i] + x[i - 4] >= x[i - 2] and x[i - 4] <= x[i - 2]:
                 return True
         return False
 
