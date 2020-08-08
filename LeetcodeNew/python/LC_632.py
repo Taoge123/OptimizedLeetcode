162d0394504e55dd3e99f5444487e1e41bc0d1f1
taocheng <taocheng984@gmail.com>
2020-08-08 11:27:23 -0700

    all

--- LeetcodeNew/python/LC_632.py
+++ LeetcodeNew/python/LC_632.py
@@ -1,5 +1,15 @@
-import heapq
+"""
+[[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
 
+4  0  5  0-5
+4  9  5  4-9
+10 9  5  5-10
+10 90 18 9-18
+
+
+"""
+
+import heapq
 
 class Solution:
     def smallestRange(self, nums):
@@ -7,14 +17,14 @@ class Solution:
         heap = [(row[0], i, 0) for i, row in enumerate(nums)]
         heapq.heapify(heap)
 
-        res = -1e9, 1e9
+        res = [float('-inf'), float('inf')]
         # get the max from first elem of all lists
         right = max(row[0] for row in nums)
 
         while heap:
             left, row, pos = heapq.heappop(heap)
             if right - left < res[1] - res[0]:
-                res = left, right
+                res = [left, right]
             print(pos + 1, len(nums[row]))
             # pos tracks index, i tracks which row
             if pos + 1 == len(nums[row]):
@@ -29,3 +39,4 @@ class Solution:
 nums = [[4,10,15,24,26], [0,9,12,20], [5,18,22,30]]
 a = Solution()
 print(a.smallestRange(nums))
+
