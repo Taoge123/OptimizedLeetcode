8f139b967573062f0121f04f936926311b34df3e
taocheng <taocheng984@gmail.com>
2020-11-07 11:52:27 -0500

    all

---
+++ LeetcodeNew/python/LC_950.py
@@ -0,0 +1,33 @@
+import collections
+
+class Solution:
+    def deckRevealedIncreasing(self, deck):
+        n = len(deck)
+        index = collections.deque(range(n))
+        res = [None] * n
+
+        for card in sorted(deck):
+            res[index.popleft()] = card
+            if index:
+                index.append(index.popleft())
+        return res
+
+
+class Solution2:
+    def deckRevealedIncreasing(self, deck):
+        n = len(deck)
+        index = collections.deque(range(n))
+        res = [None] * n
+
+        for card in sorted(deck):
+            res[index.popleft()] = card
+            if index:
+                temp = index.popleft()
+                index.append(temp)
+        return res
+
+
+deck = [17,13,11,2,3,5,7]
+a = Solution2()
+print(a.deckRevealedIncreasing(deck))
+
