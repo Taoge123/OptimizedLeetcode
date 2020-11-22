be564331ae9adaa6ea053a48174cc8aba201e12f
taocheng <taocheng984@gmail.com>
2020-11-22 12:30:20 -0500

    all

--- LeetcodeNew/python/LC_060.py
+++ LeetcodeNew/python/LC_060.py
@@ -1,4 +1,6 @@
 """
+1643. Kth Smallest Instructions
+
 The set [1,2,3,...,n] contains a total of n! unique permutations.
 
 By listing and labeling all of the permutations in order, we get the following sequence for n = 3:
