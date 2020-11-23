dac8ce6a615ad7fd64fe92113aaa8c3bced3c316
taocheng <taocheng984@gmail.com>
2020-11-22 21:52:01 -0500

    all

--- DailyChallenge/LC_1312.py
+++ DailyChallenge/LC_1312.py
@@ -1,40 +1,38 @@
-import functoolspow
 
 
-class Solution:
-    def isValidPalindrome(self, s: str, k: int) -> bool:
+
+class SolutionTony1:
+    def minInsertions(self, s: str) -> int:
 
         @functools.lru_cache(None)
         def dfs(i, j):
-            if i >= j:
+            if j - i < 1:
                 return 0
-
             if s[i] == s[j]:
-                return dfs( i +1, j- 1)
+                return dfs(i + 1, j - 1)
             else:
                 return min(dfs(i + 1, j), dfs(i, j - 1)) + 1
 
-        return dfs(0, len(s) - 1) <= k
+        return dfs(0, len(s) - 1)
 
 
 
-class Solution2:
-    def isValidPalindrome(self, s: str, k: int) -> bool:
-
+class SolutionTony2:
+    def minInsertions(self, s: str) -> int:
         memo = {}
-        return self.dfs(s, 0, len(s) - 1, memo) <= k
+        return self.dfs(s, 0, len(s) - 1, memo)
 
     def dfs(self, s, i, j, memo):
         if (i, j) in memo:
             return memo[(i, j)]
-
-        if i >= j:
+        if j - i < 1:
             return 0
-
         if s[i] == s[j]:
-            memo[(i, j)] = self.dfs(s, i+ 1, j - 1, memo)
+            memo[(i, j)] = self.dfs(s, i + 1, j - 1, memo)
         else:
             memo[(i, j)] = min(self.dfs(s, i + 1, j, memo), self.dfs(s, i, j - 1, memo)) + 1
+
         return memo[(i, j)]
+
 
 
--- LeetcodeNew/python/LC_1312.py
+++ LeetcodeNew/python/LC_1312.py
@@ -52,6 +52,44 @@ class Solution2:
 
 这么转换之后，本题就是1092.Shortest-Common-Supersequence.
 """
+import functools
+
+class SolutionTony1:
+    def minInsertions(self, s: str) -> int:
+
+        @functools.lru_cache(None)
+        def dfs(i, j):
+            if j - i < 1:
+                return 0
+            if s[i] == s[j]:
+                return dfs(i + 1, j - 1)
+            else:
+                return min(dfs(i + 1, j), dfs(i, j - 1)) + 1
+
+        return dfs(0, len(s) - 1)
+
+
+
+class SolutionTony2:
+    def minInsertions(self, s: str) -> int:
+        memo = {}
+        return self.dfs(s, 0, len(s) - 1, memo)
+
+    def dfs(self, s, i, j, memo):
+        if (i, j) in memo:
+            return memo[(i, j)]
+        if j - i < 1:
+            return 0
+        if s[i] == s[j]:
+            memo[(i, j)] = self.dfs(s, i + 1, j - 1, memo)
+        else:
+            memo[(i, j)] = min(self.dfs(s, i + 1, j, memo), self.dfs(s, i, j - 1, memo)) + 1
+
+        return memo[(i, j)]
+
+
+
+
 class Solution1:
     def minInsertions(self, s: str) -> int:
         n = len(s)
