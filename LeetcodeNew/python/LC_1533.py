6b55f1f6f84f25cd4a255167482649cec4c4c015
taocheng <taocheng984@gmail.com>
2020-11-28 11:59:19 -0500

    all

--- LeetcodeNew/python/LC_1533.py
+++ LeetcodeNew/python/LC_1533.py
@@ -3,14 +3,14 @@
 This is ArrayReader's API interface.
 You should not implement it, or speculate about its implementation
 """
-class ArrayReader(object):
-	 # Compares the sum of arr[l..r] with the sum of arr[x..y]
-	 # return 1 if sum(arr[l..r]) > sum(arr[x..y])
-	 # return 0 if sum(arr[l..r]) == sum(arr[x..y])
-	 # return -1 if sum(arr[l..r]) < sum(arr[x..y])
+class ArrayReader:
+    # Compares the sum of arr[l..r] with the sum of arr[x..y]
+	# return 1 if sum(arr[l..r]) > sum(arr[x..y])
+	# return 0 if sum(arr[l..r]) == sum(arr[x..y])
+	# return -1 if sum(arr[l..r]) < sum(arr[x..y])
    def compareSub(self, l: int, r: int, x: int, y: int) -> int:
         pass
-	 # Returns the length of the array
+    # Returns the length of the array
    def length(self) -> int:
         pass
 
