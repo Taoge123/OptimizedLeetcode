e6148e01cf8f5bf7e44457e758c01ca046d9a223
taocheng <taocheng984@gmail.com>
2020-12-19 14:39:36 -0500

    all

--- LeetcodeNew/python2/LC_1690.py
+++ LeetcodeNew/python2/LC_1690.py
@@ -7,6 +7,32 @@ class Solution:
         n = len(stones)
         memo = [[-1 for _ in range(n)] for _ in range(n)]
 
+        return self.dfs(stones, presum, 0, len(stones) - 1, memo)
+
+    def dfs(self, stones, presum, i, j, memo):
+        if i > j:
+            return 0
+
+        if memo[i][j] != -1:
+            return memo[i][j]
+
+        a = presum[j + 1] - presum[i + 1] - self.dfs(stones, presum, i + 1, j, memo)
+        b = presum[j] - presum[i] - self.dfs(stones, presum, i, j - 1, memo)
+
+        memo[i][j] = max(a, b)
+        return max(a, b)
+
+
+
+class Solution2:
+    def stoneGameVII(self, stones) -> int:
+        presum = [0]
+        for num in stones:
+            presum.append(num + presum[-1])
+
+        n = len(stones)
+        memo = [[-1 for _ in range(n)] for _ in range(n)]
+
         # memo = {}
         # @functools.lru_cache(None)
         def dfs(i, j):
