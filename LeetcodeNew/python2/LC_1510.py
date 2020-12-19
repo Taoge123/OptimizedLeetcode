2ebb18307f250a0c69ad081daa68a15f9a321899
taocheng <taocheng984@gmail.com>
2020-12-19 14:25:39 -0500

    all

--- LeetcodeNew/python2/LC_1510.py
+++ LeetcodeNew/python2/LC_1510.py
@@ -6,25 +6,26 @@ n: n-1, n-4, n-9, ... ,
 
 import math
 
+
 class Solution:
     def winnerSquareGame(self, n: int) -> bool:
-        self.cache = {}
+        self.memo = {}
         for i in range(1, n + 1):
-            self.cache[i] = -1
-        return self.solve(n)
+            self.memo[i] = -1
+        return self.dfs(n)
 
-    def solve(self, n):
-
-        if n in self.cache and self.cache[n] != -1:
-            return self.cache[n]
+    def dfs(self, n):
+        if n in self.memo and self.memo[n] != -1:
+            return self.memo[n]
 
         for i in range(1, int(math.sqrt(n)) + 1):
-            if self.solve(n - i * i) == False:
-                self.cache[n] = 1
+            if self.dfs(n - i * i) == False:
+                self.memo[n] = 1
                 return True
 
-        self.cache[n] = 0
+        self.memo[n] = 0
         return False
+
 
 
 
