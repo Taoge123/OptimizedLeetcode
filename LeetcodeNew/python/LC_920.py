e658d81b8670aeac43aa273b028a26f77b8517b7
taocheng <taocheng984@gmail.com>
2020-12-25 20:34:00 -0500

    all

--- LeetcodeNew/python/LC_920.py
+++ LeetcodeNew/python/LC_920.py
@@ -19,16 +19,19 @@ https://www.youtube.com/watch?v=uvjn6UDXVrY
 
 import functools
 
+
 class Solution:
     def numMusicPlaylists(self, N: int, L: int, K: int) -> int:
-
         mod = 10 ** 9 + 7
+
         @functools.lru_cache(None)
         def dfs(i, count):
             if i == L:
                 return count == N
 
+            # we already used count songs, we can replay a song if k other songs were played
             res = dfs(i + 1, count) * max(0, count - K)
+            # (n - count) is how many songs that we haven't used yet
             res += dfs(i + 1, count + 1) * (N - count)
 
             return res % mod
