92de4c260b6b9d2ccfcf9a6dbb5ed2e72977ce8b
taocheng <taocheng984@gmail.com>
2020-12-25 21:49:34 -0500

    all

--- LeetcodeNew/python2/LC_1307.py
+++ LeetcodeNew/python2/LC_1307.py
@@ -17,4 +17,77 @@ dfs(i, j, summ)
 
 """
 
+import collections
+
+class Solution:
+    def isSolvable(self, words, result) -> bool:
+        table = collections.defaultdict(lambda: -1)
+        visited = collections.defaultdict(int)
+        result = result[::-1]
+
+        for i, word in enumerate(words):
+            if len(word) > len(result):
+                return False
+            words[i] = word[::-1]
+
+        def dfs(i, j, summ):
+            if j == len(result):
+                if summ != 0:
+                    return False
+                if len(result) > 1 and table[result[-1]] == 0:
+                    return False
+                return True
+
+            if i == len(words):
+                if table[result[j]] != -1:
+                    if table[result[j]] != summ % 10:
+                        return False
+                    else:
+                        return dfs(0, j + 1, summ // 10)
+
+                else:
+                    if visited[summ % 10] == 1:
+                        return False
+                    table[result[j]] = summ % 10
+                    visited[summ % 10] = 1
+                    if dfs(0, j + 1, summ // 10):
+                        return True
+                    table[result[j]] = -1
+                    visited[summ % 10] = 0
+                    return False
+
+            if j >= len(words[i]):
+                return dfs(i + 1, j, summ)
+
+            ch = words[i][j]
+            if table[ch] != -1:
+                if len(words[i]) > 1 and j == len(words[i]) - 1 and table[ch] == 0:
+                    return False
+                return dfs(i + 1, j, summ + table[ch])
+
+            else:
+                for d in range(10):
+                    if visited[d] == 1:
+                        continue
+                    table[ch] = d
+                    visited[d] = 1
+                    if dfs(i + 1, j, summ + d):
+                        return True
+                    table[ch] = -1
+                    visited[d] = 0
+
+                return False
+
+        return dfs(0, 0, 0)
+
+
+
+
+words = ["SEND","MORE"]
+result = "MONEY"
+
+a = Solution()
+print(a.isSolvable(words, result))
+
+
 
