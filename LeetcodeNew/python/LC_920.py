8994762734009515d518480ebe5fff21e6e9ec2a
taocheng <taocheng984@gmail.com>
2020-12-25 20:29:02 -0500

    all

---
+++ LeetcodeNew/python/LC_920.py
@@ -0,0 +1,39 @@
+
+"""
+
+
+len, count
+
+option 1: pick one of the already used song
+    (len+1, count) * max(0, count - K)
+option 2: pick one of the already unused song
+    (len+1, count+1) * (N - count)
+
+
+length =
+count = number of unique songs
+
+https://www.youtube.com/watch?v=uvjn6UDXVrY
+
+"""
+
+import functools
+
+class Solution:
+    def numMusicPlaylists(self, N: int, L: int, K: int) -> int:
+
+        mod = 10 ** 9 + 7
+        @functools.lru_cache(None)
+        def dfs(i, count):
+            if i == L:
+                return count == N
+
+            res = dfs(i + 1, count) * max(0, count - K)
+            res += dfs(i + 1, count + 1) * (N - count)
+
+            return res % mod
+
+        return dfs(0, 0)
+
+
+
