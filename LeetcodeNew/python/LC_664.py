93262a960777cb203e27da1f3f7bdba8eb0c9bf1
taocheng <taocheng984@gmail.com>
2021-01-01 16:30:17 -0500

    all

--- LeetcodeNew/python/LC_664.py
+++ LeetcodeNew/python/LC_664.py
@@ -70,13 +70,14 @@ class SolutionDFS2:
         return self.dfs(s, 0, len(s) - 1, memo)
 
     def dfs(self, s, i, j, memo):
-
         if (i, j) in memo:
             return memo[(i, j)]
 
         if i > j:
             return 0
+
         res = self.dfs(s, i + 1, j, memo) + 1
+
         for k in range(i + 1, j + 1):
             if s[k] == s[i]:
                 res = min(res, self.dfs(s, i, k - 1, memo) + self.dfs(s, k + 1, j, memo))
