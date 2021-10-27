9f88485290f309b61735db0e864fc5a5cf63d52b
Taoge123 <taocheng984@gmail.com>
2021-10-27 00:36:12 -0400

    all

--- LeetcodeNew/python/LC_931.py
+++ LeetcodeNew/python/LC_931.py
@@ -1,4 +1,25 @@
 import copy
+import functools
+
+class SolutionTD:
+    def minFallingPathSum(self, matrix) -> int:
+
+        m, n = len(matrix), len(matrix[0])
+        @functools.lru_cache(None)
+        def dfs(i, j):
+            if j < 0 or i >= m or j >= n:
+                return float('inf')
+
+            if i == m - 1:
+                return matrix[i][j]
+
+            return matrix[i][j] + min(dfs(i + 1, j - 1), dfs(i + 1, j), dfs(i + 1, j + 1))
+
+        res = float('inf')
+        for j in range(n):
+            res = min(res, dfs(0, j))
+        return res
+
 
 
 class Solution:
