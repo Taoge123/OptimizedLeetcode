68a45e827d9851bf6afb78a5a49985c953cd0e0f
Taoge123 <taocheng984@gmail.com>
2021-12-05 12:57:52 -0800

    all

--- LeetcodeNew/python/LC_920.py
+++ LeetcodeNew/python/LC_920.py
@@ -1,5 +1,7 @@
 
 """
+https://buptwc.github.io/2018/10/08/Leetcode-920-Number-of-Music-Playlists/
+https://leetcode.com/problems/number-of-music-playlists/discuss/1579179/Python-or-Top-Down-DP-or-Well-Commented-and-easy-to-understand
 len, count
 
 option 1: pick one of the already used song
@@ -18,23 +20,126 @@ https://www.youtube.com/watch?v=uvjn6UDXVrY
 import functools
 
 
-class Solution:
-    def numMusicPlaylists(self, N: int, L: int, K: int) -> int:
+class SolutionMemo:  # super fast
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        # 建一个播放清单，总共n首歌，要播放goal首歌，每播放不同的k首歌才能重播播放前面的歌， 求有几种方式建立播放清单
+        # 每首歌都需要播放至少一次，可以重复播放
+        # goal >= n
+        # length + unique
+        # TWO CASES: add new song or add repeat song
         mod = 10 ** 9 + 7
-
         @functools.lru_cache(None)
-        def dfs(i, count):
-            if i == L:
-                return count == N
+        def dfs(count, unique):
+            if count == goal:
+                if unique == n:
+                    return 1
+                else:
+                    return 0
 
-            # we already used count songs, we can replay a song if k other songs were played
-            res = dfs(i + 1, count) * max(0, count - K)
-            # (n - count) is how many songs that we haven't used yet
-            res += dfs(i + 1, count + 1) * (N - count)
 
-            return res % mod
+            add_new = dfs(count + 1, unique + 1) * (n - unique)
+
+            add_repeat = 0
+            if unique > k:
+                add_repeat = dfs(count + 1, unique) * (unique - k)
+
+            return (add_new + add_repeat) % mod
 
         return dfs(0, 0)
 
+
+
+class SolutionMemoSlow:  # slow
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        # 建一个播放清单，总共n首歌，要播放goal首歌，每播放不同的k首歌才能重播播放前面的歌， 求有几种方式建立播放清单
+        # 每首歌都需要播放至少一次，可以重复播放
+        # goal >= n
+        # length + unique
+        # TWO CASES: add new song or add repeat song
+        mod = 10 ** 9 + 7
+        @functools.lru_cache(None)
+        def dfs(count, unique):
+
+            if count == goal:
+                return unique == n
+                #     return 1
+                # else:
+                #     return 0
+
+            # not_repeat songs --> add new song from the remaining songs (n - unique)
+            add_new = dfs(count + 1, unique + 1) * (n - unique)
+
+            # repeat songs --> add repeat song from the selected sings (unique - k) --> must meet k criterial
+            add_repeat = dfs(count + 1, unique) * max(0, unique - k)
+
+            return (add_new + add_repeat) % mod
+
+        return dfs(0, 0)
+
+
+
+
+class SolutionRika1:  # slow
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        # 建一个播放清单，总共n首歌，要播放goal首歌，每播放不同的k首歌才能重播播放前面的歌， 求有几种方式建立播放清单
+        # 每首歌都需要播放至少一次，可以重复播放
+        # goal >= n
+        # length + unique
+        # TWO CASES: add new song or add repeat song
+        memo = {}
+        return self.dfs(n, goal, k, 0, 0, memo)
+
+    def dfs(self, n, goal, k, count, unique, memo):
+        if (count, unique) in memo:
+            return memo[(count, unique)]
+
+        if count == goal:
+            if unique == n:
+                return 1
+            else:
+                return 0
+
+        mod = 10 ** 9 + 7
+
+        # not_repeat songs --> add new song from the remaining songs (n - unique)
+        add_new = self.dfs(n, goal, k, count + 1, unique + 1, memo) * (n - unique)
+
+        # repeat songs --> add repeat song from the selected sings (unique - k) --> must meet k criterial
+        add_repeat = self.dfs(n, goal, k, count + 1, unique, memo) * max(0, unique - k)
+
+        memo[(count, unique)] = (add_new + add_repeat) % mod
+        return memo[(count, unique)]
+
+
+class SolutionRika2:  # super fast
+    def numMusicPlaylists(self, n: int, goal: int, k: int) -> int:
+        # 建一个播放清单，总共n首歌，要播放goal首歌，每播放不同的k首歌才能重播播放前面的歌， 求有几种方式建立播放清单
+        # 每首歌都需要播放至少一次，可以重复播放
+        # goal >= n
+        # length + unique
+        # TWO CASES: add new song or add repeat song
+        memo = {}
+        return self.dfs(n, goal, k, 0, 0, memo)
+
+    def dfs(self, n, goal, k, count, unique, memo):
+        if (count, unique) in memo:
+            return memo[(count, unique)]
+
+        if count == goal:
+            if unique == n:
+                return 1
+            else:
+                return 0
+
+        mod = 10 ** 9 + 7
+
+        add_new = self.dfs(n, goal, k, count + 1, unique + 1, memo) * (n - unique)
+
+        add_repeat = 0
+        if unique > k:
+            add_repeat = self.dfs(n, goal, k, count + 1, unique, memo) * (unique - k)
+
+        memo[(count, unique)] = (add_new + add_repeat) % mod
+        return memo[(count, unique)]
 
 
