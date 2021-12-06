1b48cd1c988f9339068ef433fd4b6b2e01e40325
Taoge123 <taocheng984@gmail.com>
2021-12-05 19:51:25 -0800

    all

--- LeetcodeNew/python/LC_200.py
+++ LeetcodeNew/python/LC_200.py
@@ -1,24 +1,34 @@
 """
-Given a 2d grid map of '1's (land) and '0's (water), count the number of islands. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+[
+["1","1","1","1","0"],
+["1","1","0","1","0"],
+["1","1","0","0","0"],
+["0","0","0","0","0"]]
 
-Example 1:
+Input: grid = [
+  ["0","0","0","0","0"],
+  ["0","1","0","0","0"],
+  ["0","0","1","0","0"],
+  ["0","0","0","1","1"]
+]
 
-Input:
-11110
-11010
-11000
-00000
+res = 0
+for i in range(m):
+    for j in rang(n):
+        if grid[i][j] == '1':
+            res += 1
+            bfs([i, j])
+            while queue
 
-Output: 1
-Example 2:
+(0, 1) (1, 0)
 
-Input:
-11000
-11000
-00100
-00011
+Input: grid = [
+  ["1","1","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","1","1","1","1"],
+  ["1","1","1","1","1"]
+]
 
-Output: 3
 """
 
 import collections
@@ -29,24 +39,25 @@ class Solution:
             return 0
 
         m, n = len(grid), len(grid[0])
-        self.directions = [(-1, 0) ,(1, 0) ,(0, -1) ,(0, 1)]
+        self.directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
         visited = [[False for i in range(n)] for j in range(m)]
         res = 0
-        for r in range(m):
-            for c in range(n):
+        for i in range(m):
+            for j in range(n):
                 if grid[r][c] == '1' and not visited[r][c]:
-                    self.dfs(r, c, grid, m, n, visited)
+                    self.dfs(i, j, grid, m, n, visited)
                     res += 1
 
         return res
 
-    def dfs(self, r, c, grid, m, n, visited):
-        if r< 0 or c < 0 or r >= m or c >= n or grid[r][c] != '1' or visited[r][c]:
+    def dfs(self, i, j, grid, m, n, visited):
+        if i < 0 or j < 0 or i >= m or j >= n or grid[i][j] != '1' or visited[i][j]:
             return
-        visited[r][c] = True
-        for direction in self.directions:
-            x, y = r + direction[0], c + direction[1]
+        visited[i][j] = True
+        for dx, dy in self.directions:
+            x, y = i + dx, j + dy
             self.dfs(x, y, grid, m, n, visited)
+
 
 
 class SolutionBFS:
