a16e91c991c605722be3f3e01c5b8ccc7fb3637c
Taoge123 <taocheng984@gmail.com>
2022-01-22 15:06:20 -0800

    all

--- LeetcodeNew/python/LC_499.py
+++ LeetcodeNew/python/LC_499.py
@@ -72,8 +72,8 @@ class Solution:
         m, n = len(maze), len(maze[0])
         heap = [(0, "", ball[0], ball[1])]
         directions = [(-1, 0, 'u'), (1, 0, 'd'), (0, -1, 'l'), (0, 1, 'r')]
-        dp = collections.defaultdict(list)
-        dp[(ball[0], ball[1])] = [0, ""]
+        memo = collections.defaultdict(list)
+        memo[(ball[0], ball[1])] = [0, ""]
 
         while heap:
             dist, pattern, i, j = heapq.heappop(heap)
@@ -81,17 +81,17 @@ class Solution:
                 return pattern
 
             for dx, dy, d in directions:
-                step, x, y = dist, i, j
+                new_dist, x, y = dist, i, j
                 while 0 <= x + dx < m and 0 <= y + dy < n and maze[x + dx][y + dy] != 1:
                     x += dx
                     y += dy
-                    step += 1
+                    new_dist += 1
                     if [x, y] == hole:
                         break
 
-                if (x, y) not in dp or [step, pattern + d] < dp[(x, y)]:
-                    dp[(x, y)] = [step, pattern + d]
-                    heapq.heappush(heap, [step, pattern + d, x, y])
+                if (x, y) not in memo or [new_dist, pattern + d] < memo[(x, y)]:
+                    memo[(x, y)] = [new_dist, pattern + d]
+                    heapq.heappush(heap, [new_dist, pattern + d, x, y])
         return 'impossible'
 
 
