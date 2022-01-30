ac0f794c274766f8048f31232d541d73eb91608c
Taoge123 <taocheng984@gmail.com>
2022-01-30 14:54:08 -0800

    all

--- LeetcodeNew/python2/LC_1466.py
+++ LeetcodeNew/python2/LC_1466.py
@@ -1,10 +1,41 @@
 """
 https://leetcode.com/problems/reorder-routes-to-make-all-paths-lead-to-the-city-zero/discuss/661774/Python3-Easy-Short-DFS
+https://www.youtube.com/watch?v=96xJAHhOIo4
+
+
 
 """
 
 
 import collections
+
+
+class SolutionDFS2:
+    def minReorder(self, n: int, connections):
+
+        self.res = 0
+        roads = set()
+        graph = collections.defaultdict(list)
+
+        for u, v in connections:
+            roads.add((u, v))
+            graph[u].append(v)
+            graph[v].append(u)
+
+        def dfs(node):
+            visited.add(node)
+            for nei in graph[node]:
+                if nei in visited:
+                    continue
+                if (node, nei) in roads:
+                    self.res += 1
+                dfs(nei)
+
+        visited = set()
+        dfs(0)
+        return self.res
+
+
 
 
 class SolutionDFS:
