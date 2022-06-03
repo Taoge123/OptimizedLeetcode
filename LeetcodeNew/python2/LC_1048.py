b541b818ef18e1fa361989f3ff72e998b08f85d2
Taoge123 <taocheng984@gmail.com>
2022-06-03 14:08:52 -0700

    all

--- LeetcodeNew/python2/LC_1048.py
+++ LeetcodeNew/python2/LC_1048.py
@@ -1,5 +1,30 @@
 import collections
 
+class SolutionMemo:
+    def longestStrChain(self, words) -> int:
+        words = set(words)
+        memo = {}
+        res = 0
+        for word in words:
+            res = max(res, self.dfs(words, word, memo))
+
+        return res
+
+    def dfs(self, words, word, memo):
+        if word in memo:
+            return memo[word]
+
+        res = 1
+        for i in range(len(word)):
+            prev = word[:i] + word[i + 1:]
+            if prev in words:
+                res = max(res, 1 + self.dfs(words, prev, memo))
+
+        memo[word] = res
+        return memo[word]
+
+
+
 class Solution:
     def longestStrChain(self, words) -> int:
         table = {i: set() for i in range(1, 17)}
