74554408276a3d60552a7f23273e5e57a5fc4b53
Tao Cheng <taocheng@Taos-MBP.attlocal.net>
2022-06-19 15:16:31 -0700

    all

--- LeetcodeNew/python/LC_402.py
+++ LeetcodeNew/python/LC_402.py
@@ -1,45 +1,35 @@
 
-
 """
-Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+解决原题的直观思路是寻找一个尽可能小的递增序列，但要满足位数的要求。
 
-Note:
-The length of num is less than 10002 and will be ≥ k.
-The given num does not contain any leading zero.
-Example 1:
+这是栈的典型应用。利用栈维护一个递增序列。当遍历的元素小于栈顶元素时，就不断退栈直至 Stack.top()<num[i]，这样继续加入元素后仍然是一个递增序列。
 
-Input: num = "1432219", k = 3
-Output: "1219"
-Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
-Example 2:
+需要注意的几点：
 
-Input: num = "10200", k = 1
-Output: "200"
-Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
-Example 3:
+需要一个计数器count来记录退栈的数目。只有当count<k时，才进行退栈操作（即模拟删除）。
+都把所有元素都遍历结束后，如果发现仍然count<k，那么就把Stack末尾的弹出，直至count==k
+需要去除结果里的前导零。
 
-Input: num = "10", k = 2
-Output: "0"
-Explanation: Remove all the digits from the number and it is left with nothing which is 0.
 """
 
 
 class Solution:
-    def removeKdigits(self, num, k):
+    def removeKdigits(self, num: str, k: int) -> str:
 
         stack = []
         if k >= len(num):
             return "0"
 
         for item in num:
+            # we will pop when node decrease
             while stack and k and stack[-1] > item:
                 stack.pop()
                 k -= 1
             stack.append(item)
+        # if we there are still k remains, we continue to pop
         while k:
             stack.pop()
             k -= 1
-
         return str(int("".join(stack)))
 
 
