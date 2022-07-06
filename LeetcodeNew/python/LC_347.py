09c5f48003f23420671544e670040e98a83dad20
Tao Cheng <taocheng@Taos-MBP.attlocal.net>
2022-07-06 11:12:43 -0700

    all

--- LeetcodeNew/python/LC_347.py
+++ LeetcodeNew/python/LC_347.py
@@ -40,6 +40,47 @@ class Solution:
         return res
 
 
+class SolutionRika:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # 返回前k个frequency的数字 --> 用hashmap先存frequency，然后 再 存对应的值
 
+        freq = collections.defaultdict(int)
+        for num in nums:
+            freq[num] += 1
 
+        values = []
+        arr = []
+        for val in freq.keys():
+            values.append(val)
+            arr.append(freq[val])
+        # print(values, arr)
 
+        return self.quickSelect(arr, values, 0, len(arr) - 1, k)
+
+    def quickSelect(self, arr, values, left, right, k):
+        pivot = self.partition(arr, values, left, right)
+        if pivot == k - 1:
+            return values[:pivot + 1]
+        elif pivot < k - 1:
+            return self.quickSelect(arr, values, pivot + 1, right, k)
+        else:
+            return self.quickSelect(arr, values, left, pivot - 1, k)
+
+    def partition(self, arr, values, left, right):
+        if len(arr) == 0:
+            return 0
+        pivot = random.randint(left, right)
+        arr[pivot], arr[right] = arr[right], arr[pivot]
+        values[pivot], values[right] = values[right], values[pivot]
+
+        slow = left
+        for fast in range(left, right):
+            if arr[fast] > arr[right]:
+                arr[fast], arr[slow] = arr[slow], arr[fast]
+                values[fast], values[slow] = values[slow], values[fast]
+                slow += 1
+
+        arr[slow], arr[right] = arr[right], arr[slow]
+        values[slow], values[right] = values[right], values[slow]
+
+        return slow
--- LeetcodeNew/python/LC_973.py
+++ LeetcodeNew/python/LC_973.py
@@ -1,5 +1,28 @@
+import heapq
 
 class Solution:
+    def kClosest(self, points, k: int):
+        # Since heap is sorted in increasing order,
+        # negate the distance to simulate max heap
+        # and fill the heap with the first k elements of points
+        heap = [(-self.squared_distance(points[i]), i) for i in range(k)]
+        heapq.heapify(heap)
+        for i in range(k, len(points)):
+            dist = -self.squared_distance(points[i])
+            if dist > heap[0][0]:
+                # If this point is closer than the kth farthest,
+                # discard the farthest point and add this one
+                heapq.heappushpop(heap, (dist, i))
+
+        # Return all points stored in the max heap
+        return [points[i] for (_, i) in heap]
+
+    def squared_distance(self, point) -> int:
+        """Calculate and return the squared Euclidean distance."""
+        return point[0] ** 2 + point[1] ** 2
+
+
+class Solution2:
     def kClosest(self, points, K):
         points.sort(key=lambda P: P[0] ** 2 + P[1] ** 2)
         return points[:K]
