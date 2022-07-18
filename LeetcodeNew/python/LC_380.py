40e499f0ef46064bab146fddf2e0a0a2ba1dd651
Tao Cheng <taocheng@MacBook-Pro.attlocal.net>
2022-07-17 17:58:46 -0700

    all

--- LeetcodeNew/python/LC_380.py
+++ LeetcodeNew/python/LC_380.py
@@ -34,46 +34,26 @@ import random
 
 class RandomizedSet:
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.nums = []
-        self.pos = {}
-
+        self.table = {}
 
     def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
-        if val not in self.pos:
-            self.nums.append(val)
-            self.pos[val] = len(self.nums) - 1
-            return True
-        return False
+        if val in self.table:
+            return False
+        self.nums.append(val)
+        self.table[val] = len(self.nums) - 1
+        return True
 
     def remove(self, val: int) -> bool:
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        """
-        if val in self.pos:
-            idx, last = self.pos[val], self.nums[-1]
+        if val in self.table:
+            idx, last = self.table[val], self.nums[-1]
             self.nums[idx] = last
-            self.pos[last] = idx
+            self.table[last] = idx
             self.nums.pop()
-            # self.pos.pop(val)
-            del self.pos[val]
+            # self.table.pop(val)
+            del self.table[val]
             return True
         return False
 
     def getRandom(self) -> int:
-        """
-        Get a random element from the set.
-        """
         return self.nums[random.randint(0, len(self.nums) - 1)]
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
